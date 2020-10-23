9e3d1f9fb7c1e47f45f29ac2bc83c19eb255e3f3
Will Weatherill <will.weatherill@answerdigital.com>
2020-10-23 16:49:46 +0100

    Supporting dual deployment - war and fat jar

--- src/main/java/com/endavourhealth/ImViewerSpringApplication.java
+++ src/main/java/com/endavourhealth/ImViewerSpringApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,10 +19,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaRepositories("com.endavourhealth")
 @ComponentScan(basePackages = "com.endavourhealth")
 @EntityScan("com.endavourhealth")
-public class ImViewerSpringApplication {
+public class ImViewerSpringApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImViewerSpringApplication.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ImViewerSpringApplication.class);
+    }
 }

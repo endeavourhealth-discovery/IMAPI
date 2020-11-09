f9a8b86c36c6926bed63186e0dbb788791ae9cd8
Daniel Nuttall <daniel.nuttall@answerdigital.com>
2020-11-09 15:56:13 +0000

    remove "/api/"

--- api/src/main/java/com/endavourhealth/controllers/ConceptController.java
+++ api/src/main/java/com/endavourhealth/controllers/ConceptController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/concept")
+@RequestMapping("concept")
 @CrossOrigin
 public class ConceptController {
 

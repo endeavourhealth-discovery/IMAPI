093f39bbe9e0d55e6fbb1b62567493966e4a6093
David Stables <DavidStables@users.noreply.github.com>
2020-12-24 11:21:28 +0000

    Data property value handline regex and ranges

--- models/src/main/java/org/endeavourhealth/imapi/model/DataPropertyValue.java
+++ models/src/main/java/org/endeavourhealth/imapi/model/DataPropertyValue.java
@@ -4,12 +4,20 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import java.util.Set;
+
 @JsonPropertyOrder({"property","inverseOf","quantificationType","min","max","dataType","valueData"})
 public class DataPropertyValue extends QuantificationImpl{
 
    private ConceptReference property;
    private ConceptReference dataType;
    private String valueData;
+   private Set<String> oneOf;
+   private String minOperator;
+   private String minValue;
+   private String maxOperator;
+   private String maxValue;
+   private String pattern;
 
 
    @JsonProperty("Property")
@@ -49,4 +57,57 @@ public class DataPropertyValue extends QuantificationImpl{
       return this;
    }
 
+   public Set<String> getOneOf() {
+      return oneOf;
+   }
+
+   public DataPropertyValue setOneOf(Set<String> oneOf) {
+      this.oneOf = oneOf;
+      return this;
+   }
+
+   public String getMinOperator() {
+      return minOperator;
+   }
+
+   public DataPropertyValue setMinOperator(String minOperator) {
+      this.minOperator = minOperator;
+      return this;
+   }
+
+   public String getMinValue() {
+      return minValue;
+   }
+
+   public DataPropertyValue setMinValue(String minValue) {
+      this.minValue = minValue;
+      return this;
+   }
+
+   public String getMaxOperator() {
+      return maxOperator;
+   }
+
+   public DataPropertyValue setMaxOperator(String maxOperator) {
+      this.maxOperator = maxOperator;
+      return this;
+   }
+
+   public String getMaxValue() {
+      return maxValue;
+   }
+
+   public DataPropertyValue setMaxValue(String maxValue) {
+      this.maxValue = maxValue;
+      return this;
+   }
+
+   public String getPattern() {
+      return pattern;
+   }
+
+   public DataPropertyValue setPattern(String pattern) {
+      this.pattern = pattern;
+      return this;
+   }
 }

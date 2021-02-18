3f1d131aac1667406cfc7a55c6e5524cdd608fed
Daniel Nuttall <daniel.nuttall@answerdigital.com>
2021-02-18 17:23:09 +0000

    Update IMLangListener to generate a Concept as the tree is processed.

---
+++ api/src/main/java/org/endeavourhealth/converters/IMLangListener.java
@@ -0,0 +1,47 @@
+package org.endeavourhealth.converters;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.endeavourhealth.imapi.model.Concept;
+import org.endeavourhealth.informationmanager.parser.IMLangBaseListener;
+import org.endeavourhealth.informationmanager.parser.IMLangParser.CodeContext;
+import org.endeavourhealth.informationmanager.parser.IMLangParser.DescriptionContext;
+import org.endeavourhealth.informationmanager.parser.IMLangParser.IdentifierIriContext;
+import org.endeavourhealth.informationmanager.parser.IMLangParser.NameContext;
+
+public class IMLangListener extends IMLangBaseListener {
+	
+	Concept concept = new Concept();
+	
+	@Override
+	public void enterEveryRule(ParserRuleContext ctx) { // see gramBaseListener for allowed functions
+		System.out.println("rule entered: " + ctx.getText()); // code that executes per rule
+	}
+	
+	@Override
+	public void enterIdentifierIri(IdentifierIriContext ctx) {
+		concept.setIri(ctx.getText());
+	}
+	
+	@Override
+	public void enterName(NameContext ctx) {
+		concept.setName(ctx.getText().replace("Name ", "").replaceAll("\"", ""));
+	}
+	
+	@Override
+	public void enterDescription(DescriptionContext ctx) {
+		concept.setDescription(ctx.getText().replace("description ", "").replaceAll("\"", ""));
+	}
+	
+	@Override
+	public void enterCode(CodeContext ctx) {
+		concept.setCode(ctx.getText().replace("code", "").replaceAll("\"", ""));
+	}
+
+	public Concept getConcept() {
+		return concept;
+	}
+
+	public void setConcept(Concept concept) {
+		this.concept = concept;
+	}
+}
--- api/src/main/java/org/endeavourhealth/converters/ImLangToConcept.java
+++ api/src/main/java/org/endeavourhealth/converters/ImLangToConcept.java
@@ -1,12 +1,10 @@
 package org.endeavourhealth.converters;
 
-import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.endeavourhealth.imapi.model.ClassExpression;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.endeavourhealth.imapi.model.Concept;
-import org.endeavourhealth.imapi.model.ConceptStatus;
-import org.endeavourhealth.imapi.model.ConceptType;
 import org.endeavourhealth.informationmanager.parser.IMLangLexer;
 import org.endeavourhealth.informationmanager.parser.IMLangParser;
 import org.springframework.stereotype.Component;
@@ -18,50 +16,48 @@ public class ImLangToConcept {
 
 		System.out.println(conceptDefinition);
 
-		CharStream in = CharStreams.fromString(conceptDefinition);
-		IMLangLexer lexer = new IMLangLexer(in);
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		IMLangParser parser = new IMLangParser(tokens);
+		IMLangLexer lexer = new IMLangLexer(CharStreams.fromString(conceptDefinition));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        IMLangParser parser = new IMLangParser(tokens);
+        
+        parser.setBuildParseTree(true);
+        
+        ParseTree tree = parser.concept(); // parse the content and get the tree
+        IMLangListener listener = new IMLangListener();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(listener,tree);
 
-		Concept concept = new Concept();
-		concept.setIri(parser.iri().getText());
-		concept.setName(parser.name().getText());
-		concept.setDescription(parser.description().getText());
-		concept.setCode(parser.code().getText());
-		concept.setConceptType(ConceptType.byName(parser.type().getText()));
-		concept.setStatus(ConceptStatus.byName(parser.status().getText()));
+//		// SubclassOf
+//		if (!parser.subclassOf().getText().isEmpty()) {
+//			String[] subclasses = parser.subclassOf().getText().split(",");
+//			for (String subclass : subclasses) {
+//				ClassExpression classExpression = new ClassExpression();
+//				classExpression.setClazz(subclass);
+//				concept.addSubClassOf(classExpression);
+//			}
+//		}
+//
+//		// Members
+//		if (!parser.members().getText().isEmpty()) {
+//			String[] members = parser.members().getText().split(",");
+//			for (String member : members) {
+//				ClassExpression classExpression = new ClassExpression();
+//				classExpression.setClazz(member);
+//				concept.addMember(classExpression);
+//			}
+//		}
+//
+//		// EquivalentTo
+//		if (!parser.equivalentTo().getText().isEmpty()) {
+//			String[] equivalentTo = parser.equivalentTo().getText().split(",");
+//			for (String equivalent : equivalentTo) {
+//				ClassExpression classExpression = new ClassExpression();
+//				classExpression.setClazz(equivalent);
+//				concept.addEquivalentTo(classExpression);
+//			}
+//		}
 
-		// SubclassOf
-		if (!parser.subclassOf().getText().isEmpty()) {
-			String[] subclasses = parser.subclassOf().getText().split(",");
-			for (String subclass : subclasses) {
-				ClassExpression classExpression = new ClassExpression();
-				classExpression.setClazz(subclass);
-				concept.addSubClassOf(classExpression);
-			}
-		}
-
-		// Members
-		if (!parser.members().getText().isEmpty()) {
-			String[] members = parser.members().getText().split(",");
-			for (String member : members) {
-				ClassExpression classExpression = new ClassExpression();
-				classExpression.setClazz(member);
-				concept.addMember(classExpression);
-			}
-		}
-
-		// EquivalentTo
-		if (!parser.equivalentTo().getText().isEmpty()) {
-			String[] equivalentTo = parser.equivalentTo().getText().split(",");
-			for (String equivalent : equivalentTo) {
-				ClassExpression classExpression = new ClassExpression();
-				classExpression.setClazz(equivalent);
-				concept.addEquivalentTo(classExpression);
-			}
-		}
-
-		return concept;
+		return listener.getConcept();
 	}
 
 }

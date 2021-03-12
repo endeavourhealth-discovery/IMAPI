44b9905d7d6a04a0ed37508b99c5761b295f75e2
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-03-12 11:08:36 +0000

    Switch literal type from string to iri ref

--- models/src/main/java/org/endeavourhealth/imapi/model/tripletree/TTLiteral.java
+++ models/src/main/java/org/endeavourhealth/imapi/model/tripletree/TTLiteral.java
@@ -1,22 +1,38 @@
 package org.endeavourhealth.imapi.model.tripletree;
 
+import static org.endeavourhealth.imapi.model.tripletree.TTIriRef.iri;
+
 public class TTLiteral extends TTValue {
     public static TTLiteral literal(String value) {
         return new TTLiteral(value);
+    }
+
+    public static TTLiteral literal(String value, TTIriRef type) {
+        return new TTLiteral(value, type);
     }
 
     public static TTLiteral literal(String value, String type) {
         return new TTLiteral(value, type);
     }
 
+    public static TTLiteral literal(String value, String type, String typeName) {
+        return new TTLiteral(value, type, typeName);
+    }
+
     String value;
-    String type;
+    TTIriRef type;
 
     public TTLiteral() {}
     public TTLiteral(String value) {
         this.value = value;
     }
     public TTLiteral(String value, String type) {
+        this(value, iri(type));
+    }
+    public TTLiteral(String value, String type, String typeName) {
+        this(value, iri(type, typeName));
+    }
+    public TTLiteral(String value, TTIriRef type) {
         this.value = value;
         this.type = type;
     }

620d471929c9b79a811d99100fd0792db05cb403
Zeynep Bayazitoglu <zeynep.bayazitoglu@ergosoft.co.uk>
2021-04-22 12:10:51 +0100

    Add time cache to ReportService

--- dataaccess/src/main/java/org/endeavourhealth/dataaccess/ReportService.java
+++ dataaccess/src/main/java/org/endeavourhealth/dataaccess/ReportService.java
@@ -3,55 +3,74 @@ package org.endeavourhealth.dataaccess;
 
 import org.endeavourhealth.dataaccess.repository.SimpleCountRepository;
 import org.endeavourhealth.imapi.model.report.SimpleCount;
+import org.endeavourhealth.imapi.model.report.SimpleCountCache;
 import org.endeavourhealth.imapi.vocabulary.IM;
 import org.endeavourhealth.imapi.vocabulary.OWL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @Qualifier("ReportService")
 public class ReportService implements IReportService{
 
+    private interface ReportServiceLambda {
+        List<SimpleCount> execute();
+    }
+
+    private static final Map<String, SimpleCountCache>  cache = new HashMap<>();
+
     @Autowired
     SimpleCountRepository simpleCountRepository;
 
-    @Override
-    public List<SimpleCount> getConceptTypeReport() {
-        List<SimpleCount> list = new ArrayList<>();
-        for (org.endeavourhealth.dataaccess.entity.SimpleCount t : simpleCountRepository.getConceptTypeReport()) {
-            SimpleCount simpleCount = new SimpleCount(t.getLabel(), t.getCount());
-            list.add(simpleCount);
+    private List<SimpleCount> getAndCacheReport(String reportType, ReportServiceLambda serviceCall) {
+        SimpleCountCache report = cache.get(reportType);
+        if(report!=null&&!report.cacheOlderThan(30L)){
+            return report.getData();
         }
+        List<SimpleCount> list = serviceCall.execute();
+
+        cache.put(reportType,new SimpleCountCache(list));
+        cache.get(reportType).setDateTime(LocalDateTime.now());
         return list;
     }
 
     @Override
-    public List<SimpleCount> getConceptSchemeReport() {
+    public List<SimpleCount> getConceptTypeReport() {
+      return  getAndCacheReport("Concept Type", () -> simpleCountRepository.getConceptTypeReport()
+              .stream()
+              .map(sc -> new SimpleCount(sc.getLabel(), sc.getCount()))
+              .collect(Collectors.toList()));
+    }
 
-        return simpleCountRepository.getConceptSchemeReport()
-            .stream()
-            .map(s -> new SimpleCount(s.getLabel(),s.getCount()))
-            .collect(Collectors.toList());
+    @Override
+    public List<SimpleCount> getConceptSchemeReport() {
+        return  getAndCacheReport("Concept Scheme", () -> simpleCountRepository.getConceptSchemeReport().stream()
+                .map(sc -> new SimpleCount(sc.getLabel(), sc.getCount()))
+                .collect(Collectors.toList()));
     }
 
     @Override
     public List<SimpleCount> getConceptStatusReport() {
-
-        return simpleCountRepository.getConceptStatusReport()
-            .stream()
-            .map(s -> new SimpleCount(s.getLabel(),s.getCount()))
-            .collect(Collectors.toList());
+        return  getAndCacheReport("Concept Status", () -> simpleCountRepository.getConceptStatusReport().stream()
+                .map(sc -> new SimpleCount(sc.getLabel(), sc.getCount()))
+                .collect(Collectors.toList()));
     }
 
     @Override
     public List<SimpleCount> getConceptCategoryReport() {
-        List<SimpleCount> result = new ArrayList<>();
+       return getAndCacheReport("Concept Category", this::getConceptCategoryReportData);
+    }
 
+    private List<SimpleCount> getConceptCategoryReportData() {
+        List<SimpleCount> result = new ArrayList<>();
         int dmCount = 0;
         int ontCount = 0;
         int vsCount =0;
@@ -71,7 +90,6 @@ public class ReportService implements IReportService{
         result.add(new SimpleCount("Data Models",dmCount));
         result.add(new SimpleCount("Ontology",ontCount));
         result.add(new SimpleCount("Total",vsCount+dmCount+ontCount));
-
         return result;
     }
 }

93f406f04c56c061e4bc00d25c6184a7190abd64
Zeynep Bayazitoglu <zeynep.bayazitoglu@ergosoft.co.uk>
2021-04-29 13:42:24 +0100

    Make tree items in alphabetical order

--- dataaccess/src/main/java/org/endeavourhealth/dataaccess/repository/ConceptTripleRepository.java
+++ dataaccess/src/main/java/org/endeavourhealth/dataaccess/repository/ConceptTripleRepository.java
@@ -23,7 +23,8 @@ public interface ConceptTripleRepository extends JpaRepository<Tpl, String> {
             "JOIN concept p ON p.dbid = t.predicate AND p.iri IN " +
             "('http://endhealth.info/im#isA', 'http://endhealth.info/im#isContainedIn') " +
             "JOIN concept o ON o.dbid = t.object " +
-            "WHERE c.iri = :iri ", nativeQuery = true)
+            "WHERE c.iri = :iri " +
+            "ORDER BY o.name ", nativeQuery = true)
     List<Tpl> findImmediateParentsByIri(String iri, Pageable pageable);
 
     @Query(value = "SELECT t.* " +
@@ -32,17 +33,9 @@ public interface ConceptTripleRepository extends JpaRepository<Tpl, String> {
             "JOIN concept p ON p.dbid = t.predicate AND p.iri IN " +
             "('http://endhealth.info/im#isA', 'http://endhealth.info/im#isContainedIn') " +
             "JOIN concept o ON o.dbid = t.subject " +
-            "WHERE c.iri = :iri  ", nativeQuery = true)
+            "WHERE c.iri = :iri  " +
+            "ORDER BY o.name ", nativeQuery = true)
     List<Tpl> findImmediateChildrenByIri(String iri, Pageable pageable);
-
-    @Query(value = "SELECT t.* " +
-            "FROM concept c " +
-            "JOIN tpl t ON t.object = c.dbid " +
-            "JOIN concept p ON p.dbid = t.predicate AND p.iri IN " +
-            "('http://endhealth.info/im#isA', 'http://endhealth.info/im#isContainedIn') " +
-            "JOIN concept o ON o.dbid = t.subject " +
-            "WHERE c.iri = :iri  ", nativeQuery = true)
-    List<Tpl> findImmediateChildrenByIri(String iri);
 
     @Modifying
     @Query(value = "INSERT INTO tpl (subject, graph, group_number, predicate, object) " +

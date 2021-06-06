2b9f8c96b711974d6d82b9f947a43d232c2e4ab8
David Stables <DavidStables@users.noreply.github.com>
2021-06-06 11:38:50 +0100

    Vocabs for Apex pathology code scheme

---
+++ models/src/main/java/org/endeavourhealth/imapi/vocabulary/APK.java
@@ -0,0 +1,7 @@
+package org.endeavourhealth.imapi.vocabulary;
+
+public class APK {
+	public static final String NAMESPACE = "http://endhealth.info/APEXK#";
+	public static final String PREFIX = "apk";
+}
+

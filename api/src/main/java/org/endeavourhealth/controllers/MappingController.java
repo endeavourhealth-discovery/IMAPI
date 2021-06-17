3469da309c5e77b7411cf5489963350dfe58b1c1
Kostas Kardatos <kostas.kardatos@answerdigital.com>
2021-06-17 17:25:14 +0100

    fix unnecessary Mockito stubbings error
    add rml tool + mapping for lsoa 2011
    
    conditional mapping for csv
    
    map csv to ttconcepts - use subset of csv for test
    
    add prsb json processing
    
    load file and map from request
    
    set file size limit
    
    check if file exists before writing to disk

---
+++ api/src/main/java/org/endeavourhealth/controllers/MappingController.java
@@ -0,0 +1,133 @@
+package org.endeavourhealth.controllers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
+import org.endeavourhealth.imapi.model.tripletree.TTArray;
+import org.endeavourhealth.imapi.model.tripletree.TTConcept;
+import org.endeavourhealth.imapi.model.tripletree.TTIriRef;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import be.ugent.rml.Executor;
+import be.ugent.rml.Utils;
+import be.ugent.rml.functions.FunctionLoader;
+import be.ugent.rml.functions.lib.IDLabFunctions;
+import be.ugent.rml.records.RecordsFactory;
+import be.ugent.rml.store.Quad;
+import be.ugent.rml.store.QuadStore;
+import be.ugent.rml.store.QuadStoreFactory;
+import be.ugent.rml.store.RDF4JStore;
+import be.ugent.rml.term.NamedNode;
+import io.swagger.annotations.Api;
+
+@RestController
+@RequestMapping("api/mapping")
+@CrossOrigin(origins = "*")
+@Api(value = "MappingController", description = "Mapping endpoint")
+public class MappingController {
+
+	ObjectMapper mapper = new ObjectMapper();
+
+	@PostMapping
+	public List<TTConcept> main(@RequestParam("file") MultipartFile file, @RequestParam("map") MultipartFile map)
+			throws Exception {
+		List<TTConcept> ttconcepts = new ArrayList<>();
+		ttconcepts.addAll(map(file, map));
+		return ttconcepts;
+	}
+
+	private List<TTConcept> map(MultipartFile file, MultipartFile map) throws Exception {
+		// path to the content file
+		File savedFile = new File("src/test/resources/" + file.getResource().getFilename());
+		if (!savedFile.exists()) {
+			file.transferTo(new File(savedFile.getAbsolutePath()));
+		}
+		
+		// path to the mapping file that needs to be executed
+		File savedMap = new File("src/test/resources/" + map.getResource().getFilename());
+		map.transferTo(new File(savedMap.getAbsolutePath()));
+
+		// Get the mapping string stream
+		InputStream mappingStream = new FileInputStream(savedMap);
+
+		// Load the mapping in a QuadStore
+		QuadStore rmlStore = QuadStoreFactory.read(mappingStream);
+
+		// Set up the basepath for the records factory, i.e., the basepath for the
+		// (local file) data sources
+		RecordsFactory factory = new RecordsFactory(savedMap.getParent());
+
+		// Set up the functions used during the mapping
+		Map<String, Class> libraryMap = new HashMap<>();
+		libraryMap.put("IDLabFunctions", IDLabFunctions.class);
+
+		FunctionLoader functionLoader = new FunctionLoader(null, libraryMap);
+
+		// Set up the outputstore (needed when you want to output something else than
+		// nquads
+		QuadStore outputStore = new RDF4JStore();
+
+		// Create the Executor
+		Executor executor = new Executor(rmlStore, factory, functionLoader, outputStore,
+				Utils.getBaseDirectiveTurtle(mappingStream));
+
+		// Execute the mapping
+		QuadStore result = executor.executeV5(null).get(new NamedNode("rmlmapper://default.store"));
+		result.getQuads(null, null, null).forEach(quad -> {
+			String subject = quad.getSubject().getValue();
+			String predicate = quad.getPredicate().getValue();
+			String object = quad.getObject().getValue();
+			System.out.println(subject + " : " + predicate + " : " + object);
+		});
+
+		List<TTConcept> ttconcepts = getTTConceptsFromQuads(result.getQuads(null, null, null));
+
+		savedFile.delete();
+		savedMap.delete();
+		
+		return ttconcepts;
+	}
+
+	private List<TTConcept> getTTConceptsFromQuads(List<Quad> quads) throws JsonProcessingException {
+		List<TTConcept> concepts = new ArrayList<>();
+
+		quads.forEach(quad -> {
+			List<TTConcept> existingConcepts = concepts.stream()
+					.filter(concept -> quad.getSubject().getValue().equals(concept.getIri()))
+					.collect(Collectors.toList());
+
+			if (!existingConcepts.isEmpty()) {
+				existingConcepts.get(0).addObject(new TTIriRef(quad.getPredicate().getValue()),
+						new TTIriRef(quad.getObject().getValue()));
+			} else {
+				TTConcept concept = new TTConcept().setIri(quad.getSubject().getValue());
+				concept.addObject(new TTIriRef(quad.getPredicate().getValue()),
+						new TTIriRef(quad.getObject().getValue()));
+				concepts.add(concept);
+			}
+		});
+
+		return concepts;
+	}
+}

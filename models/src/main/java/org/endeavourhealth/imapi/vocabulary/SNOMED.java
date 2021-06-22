04c5e5e5f8f15ffce6e02d9ac8b3d1157a26dd1d
David Stables <DavidStables@users.noreply.github.com>
2021-06-22 22:35:19 +0100

    Wrong snomed graph iri with snomed importer

--- models/src/main/java/org/endeavourhealth/imapi/vocabulary/SNOMED.java
+++ models/src/main/java/org/endeavourhealth/imapi/vocabulary/SNOMED.java
@@ -7,6 +7,5 @@ import static org.endeavourhealth.imapi.model.tripletree.TTIriRef.iri;
 public class SNOMED {
     public static final String NAMESPACE = "http://snomed.info/sct#";
     public static final String PREFIX = "sn";
-    public static final TTIriRef GRAPH= iri("http://snomed.info/sct");
     public static final TTIriRef REPLACED_BY= iri("http://snomed.info/sct#370124000");
 }

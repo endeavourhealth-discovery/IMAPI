9bf1dc69a7a082dd88760e184be85b41e8fbe90f
Kostas Kardatos <kostas.kardatos@answerdigital.com>
2021-08-19 16:56:02 +0100

    check TTValue in set() for blank values or empty lists

--- models/src/main/java/org/endeavourhealth/imapi/model/tripletree/TTNode.java
+++ models/src/main/java/org/endeavourhealth/imapi/model/tripletree/TTNode.java
@@ -11,95 +11,99 @@ import java.util.*;
 @JsonSerialize(using = TTNodeSerializerV2.class)
 @JsonDeserialize(using = TTNodeDeserializerV2.class)
 public class TTNode implements TTValue {
-    private Map<TTIriRef, TTValue> predicateValues = new HashMap<>();
+	private Map<TTIriRef, TTValue> predicateValues = new HashMap<>();
 
-    public TTNode set(TTIriRef predicate, TTValue value) {
-        if (value==null)
-            predicateValues.remove(predicate);
-        else
-            predicateValues.put(predicate, value);
-        return this;
-    }
-    
-    public TTNode remove(TTIriRef predicate) {
-    	predicateValues.remove(predicate);
-    	return this;
-    }
+	public TTNode set(TTIriRef predicate, TTValue value) {
+		if (value == null || value.isLiteral() && value.asLiteral().getValue().isBlank()
+				|| value.isIriRef() && value.asIriRef().getIri().isBlank()
+				|| value.isList() && value.asArray().getElements().isEmpty())
+			predicateValues.remove(predicate);
+		else
+			predicateValues.put(predicate, value);
+		return this;
+	}
 
-    public TTValue get(TTIriRef predicate) {
-        return predicateValues.get(predicate);
-    }
+	public TTNode remove(TTIriRef predicate) {
+		predicateValues.remove(predicate);
+		return this;
+	}
 
-    public boolean has(TTIriRef predicate) {
-        return predicateValues.containsKey(predicate);
-    }
+	public TTValue get(TTIriRef predicate) {
+		return predicateValues.get(predicate);
+	}
 
-    public Map<TTIriRef, TTValue> getPredicateMap() {
-        return this.predicateValues;
-    }
+	public boolean has(TTIriRef predicate) {
+		return predicateValues.containsKey(predicate);
+	}
 
-    public TTNode setPredicateMap(Map<TTIriRef,TTValue> predicateMap) {
-        this.predicateValues= predicateMap;
-        return this;
-    }
+	public Map<TTIriRef, TTValue> getPredicateMap() {
+		return this.predicateValues;
+	}
 
-    @Override
-    public TTNode asNode() {
-        return this;
-    }
+	public TTNode setPredicateMap(Map<TTIriRef, TTValue> predicateMap) {
+		this.predicateValues = predicateMap;
+		return this;
+	}
 
-    @Override
-    @JsonIgnore
-    public boolean isNode() {
-        return true;
-    }
+	@Override
+	public TTNode asNode() {
+		return this;
+	}
 
-    public TTLiteral getAsLiteral(TTIriRef predicate) {
-        return (TTLiteral) predicateValues.get(predicate);
-    }
+	@Override
+	@JsonIgnore
+	public boolean isNode() {
+		return true;
+	}
 
-    public TTIriRef getAsIriRef(TTIriRef predicate) {
-        return (TTIriRef) predicateValues.get(predicate);
-    }
+	public TTLiteral getAsLiteral(TTIriRef predicate) {
+		return (TTLiteral) predicateValues.get(predicate);
+	}
 
-    public TTArray getAsArray(TTIriRef predicate) {
-        return predicateValues.get(predicate).asArray();
-    }
+	public TTIriRef getAsIriRef(TTIriRef predicate) {
+		return (TTIriRef) predicateValues.get(predicate);
+	}
 
-    @Override
-    public TTArray asArray(){
-        return new TTArray().add(this);
-    }
+	public TTArray getAsArray(TTIriRef predicate) {
+		return predicateValues.get(predicate).asArray();
+	}
 
-    @Override
-    public List<TTValue> getElements(){
-        return new ArrayList<>(Collections.singletonList(this));
-    }
+	@Override
+	public TTArray asArray() {
+		return new TTArray().add(this);
+	}
 
-    public List<TTValue> getAsArrayElements(TTIriRef predicate) {
-        return predicateValues.get(predicate).getElements();
-    }
+	@Override
+	public List<TTValue> getElements() {
+		return new ArrayList<>(Collections.singletonList(this));
+	}
 
+	public List<TTValue> getAsArrayElements(TTIriRef predicate) {
+		return predicateValues.get(predicate).getElements();
+	}
 
-    public TTNode getAsNode(TTIriRef predicate) {
-        return (TTNode) predicateValues.get(predicate);
-    }
+	public TTNode getAsNode(TTIriRef predicate) {
+		return (TTNode) predicateValues.get(predicate);
+	}
 
-    /**
-     * Adds an object to a predicate if necessary converting to an array if not already an array
-     * @param predicate the predicate to add the object to. This may or may not already exist
-     * @return the modified node with a predicate object as an array
-     */
+	/**
+	 * Adds an object to a predicate if necessary converting to an array if not
+	 * already an array
+	 * 
+	 * @param predicate the predicate to add the object to. This may or may not
+	 *                  already exist
+	 * @return the modified node with a predicate object as an array
+	 */
 
-    public TTNode addObject(TTIriRef predicate, TTValue object){
-        if (this.get(predicate)==null)
-            this.set(predicate,new TTArray().add(object));
-        else if (!this.get(predicate).isList()){
-            TTValue oldObject= this.get(predicate);
-            this.set(predicate,new TTArray().add(oldObject).add(object));
-        } else
-            this.get(predicate).asArray().add(object);
-        return this;
-    }
+	public TTNode addObject(TTIriRef predicate, TTValue object) {
+		if (this.get(predicate) == null)
+			this.set(predicate, new TTArray().add(object));
+		else if (!this.get(predicate).isList()) {
+			TTValue oldObject = this.get(predicate);
+			this.set(predicate, new TTArray().add(oldObject).add(object));
+		} else
+			this.get(predicate).asArray().add(object);
+		return this;
+	}
 
 }

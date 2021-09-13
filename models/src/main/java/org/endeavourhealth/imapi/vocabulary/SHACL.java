b4456943774d863776f8aef353fd87d49c6e784e
David Stables <DavidStables@users.noreply.github.com>
2021-09-13 15:24:23 +0100

    Transform module now in place moved from info manager

--- models/src/main/java/org/endeavourhealth/imapi/vocabulary/SHACL.java
+++ models/src/main/java/org/endeavourhealth/imapi/vocabulary/SHACL.java
@@ -7,6 +7,7 @@ import static org.endeavourhealth.imapi.model.tripletree.TTIriRef.iri;
 public class SHACL {
    public static final String NAMESPACE = "http://www.w3.org/ns/shacl#";
    public static final String PREFIX = "sh";
+   public static final TTIriRef PATH= iri(NAMESPACE +"path");
    public static final TTIriRef MININCLUSIVE= iri(NAMESPACE +"minInclusive");
    public static final TTIriRef MINEXCLUSIVE= iri(NAMESPACE +"minExclusive");
    public static final TTIriRef MAXINCLUSIVE= iri(NAMESPACE +"maxInclusive");
@@ -16,7 +17,6 @@ public class SHACL {
    public static final TTIriRef MAXCOUNT= iri(NAMESPACE +"maxCount");
    public static final TTIriRef HASVALUE= iri(NAMESPACE +"hasValue");
    public static final TTIriRef PATTERN= iri(NAMESPACE +"pattern");
-   public static final TTIriRef PATH= iri(NAMESPACE +"path");
    public static final TTIriRef INVERSEPATH= iri(NAMESPACE +"inversePath");
    public static final TTIriRef CLASS= iri(NAMESPACE +"class");
    public static final TTIriRef DATATYPE= iri(NAMESPACE +"datatype");

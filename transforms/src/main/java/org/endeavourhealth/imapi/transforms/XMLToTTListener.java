f42af7d39e32ab51640f0010db1827369e71bfc2
David Stables <DavidStables@users.noreply.github.com>
2021-11-15 14:42:58 +0000

    EMIS duplicate subclasses removed

--- transforms/src/main/java/org/endeavourhealth/imapi/transforms/XMLToTTListener.java
+++
@@ -1,9 +0,0 @@
-package org.endeavourhealth.imapi.transforms;
-
-import org.endeavourhealth.imapi.model.tripletree.TTDocument;
-import org.endeavourhealth.imapi.model.tripletree.TTEntity;
-
-public interface XMLToTTListener {
-	void startElement(String path, TTEntity entity, TTDocument document);
-	void endElement(String path, TTEntity entity, TTDocument document);
-}

eda3bb4aab579d86cffc15dbed5d2c5ebf848c14
David Stables <DavidStables@users.noreply.github.com>
2021-11-26 15:52:06 +0000

    Query Model

--- models/src/main/java/org/endeavourhealth/imapi/query/Query.java
+++ models/src/main/java/org/endeavourhealth/imapi/query/Query.java
@@ -1,103 +1,86 @@
 package org.endeavourhealth.imapi.query;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.endeavourhealth.imapi.model.tripletree.*;
+import org.endeavourhealth.imapi.vocabulary.IM;
 import org.endeavourhealth.imapi.vocabulary.IMQ;
+import org.endeavourhealth.imapi.vocabulary.RDF;
 
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.zip.DataFormatException;
 
 /**
-	* A specialised RDF node that provides methods to build a query.
-	* Provides the node as a TTNode thus serializable as JSON-LD or Turtle
+	* A serializable query object containing a sequence of clauses resulting in the definition of a  data set output.
+	* <p> In effect a high level process model taylored to the health query ues cases</p>
 	*/
-public class Query extends TTNode {
 
-	public Query(){}
+@JsonPropertyOrder({"prefixes","mainEntity","clauses"})
+public class Query {
+	private final Map<String,String> prefixes = new HashMap<>();
+	private final transient Map<String,String> prefixMap= new HashMap<>();
+	private String mainEntity;
+	private List<QueryClause> clauses = new ArrayList<>();
 
-	/**
-	 * Instantiates a query from an RDF pre-populated node
-	 * @param source the pre-populaed RDF query node
+
+
+	/** Returns the list of clauses that are logically sequential
+	 *
+	 * @return List of clauses
 	 */
-	public Query(TTNode source){
-		this.setPredicateMap(source.getPredicateMap());
+	public List<QueryClause> getClauses() {
+		return clauses;
 	}
 
-
-
-	/**
-	 * selects or creates a select clause
-	 * @return the select clause as an array.
+	/** Adds a list of clauses to the query
+	 * @param clauses The list of clauses, list order indicating the logical sequence
+	 * @return the modified query
 	 */
-	public TTArray addSelectVar(String var) throws DataFormatException {
-		addExpressionAs(var,null,null);
-		if (get(IMQ.SELECT)==null)
-			set(IMQ.SELECT,new TTArray());
-		return get(IMQ.SELECT).asArray();
-	}
-
-
-	/**
-	 * Adds a variable and 'as' to select clause
-	 * @param var variable name without ? or $
-	 * @param as projection (column) alias wihout ? or $
-	 * @return the select clause
-	 */
-	public TTArray addSelectVarAs(String var,String as) throws DataFormatException {
-		return addExpressionAs(var,as,null);
-	}
-	/**
-	 * Adds a select variable and/ or expression with optional field name 'as'
-	 * @param var  variable name (without the ? or $)
-	 * @param as  column name or projection name (wihout the ? or $)
-	 * @param expression (expression string)
-	 * @return select for chaining
-	 */
-	public TTArray addExpressionAs(String var, String as, String expression) throws DataFormatException {
-		TTArray select = setSelect();
-		TTNode expAs= new TTNode();
-		if (expression!=null && as==null)
-			throw new DataFormatException("Must have column name for expression");
-		if (as!=null && var==null && expression==null)
-			throw new DataFormatException("Must have variable or expression for column name");
-		select.add(expAs);
-		if (var!=null)
-			expAs.set(IMQ.VAR, TTLiteral.literal(var));
-		if (as!=null)
-			expAs.set(IMQ.AS,TTLiteral.literal(as));
-		if (expression!=null)
-			expAs.set(IMQ.EXPRESSION,TTLiteral.literal(expression));
-		return get(IMQ.SELECT).asArray();
-	}
-	public TTArray setSelect(){
-		if (get(IMQ.SELECT)==null)
-			set(IMQ.SELECT,new TTArray());
-		return get(IMQ.SELECT).asArray();
-
+	public Query setClauses(List<QueryClause> clauses) {
+		this.clauses = clauses;
+		return this;
 	}
 
 	/**
-	 * Sets or creates a where clause
-	 * @return where clause
+	 * Adds a clause to the query
+	 * @param clause the clause to be added
+	 * @return the updated query
 	 */
-	public QueryClause setWhere(){
-		if (get(IMQ.WHERE) == null)
-			set(IMQ.WHERE, new QueryClause());
-		return (QueryClause) get(IMQ.WHERE);
+	public Query addClause(QueryClause clause){
+		this.clauses.add(clause);
+		return this;
+	}
+
+	public String getMainEntity() {
+		return mainEntity;
+	}
+
+	public Query setMainEntity(String mainEntity) {
+		this.mainEntity = mainEntity;
+		return this;
 	}
 
 
-
-
-	/**
-	 * Adds a variable to any node
-	 * @param node filter clause to add the variable to
-	 * @param var the name of the variable without ?/$
-	 * @return the filter clause
-	 */
-
-	public TTNode addVar(TTNode node,String var){
-		node.set(IMQ.VAR,TTLiteral.literal(var));
-		return node;
+	@JsonIgnore
+	public String toJson() throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
+		String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+		return json;
 	}
 
+	public Map<String, String> getPrefixes() {
+		return prefixes;
+	}
 
+	@JsonIgnore
+	public Map<String, String> getPrefixMap() {
+		return prefixMap;
+	}
 }

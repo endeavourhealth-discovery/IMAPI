618e74002ab1e01621b8e9e4bd03916b5c026080
Kostas Kardatos <kon.kardatos@gmail.com>
2021-12-02 10:47:05 +0200

    fix replaceAll + .gitignore node modules

--- nodets/controllers/ParseController.ts
+++ nodets/controllers/ParseController.ts
@@ -1,14 +1,14 @@
 export function getJsonFromFile(fileString: string): any[] {
   const lines = fileString.split("\n");
   const jsonArray = [] as any[];
-  const headers = lines[0].split(",").map(header => header.replaceAll('"', ""));
+  const headers = lines[0].split(",").map(header => header.replace(/"+/g, ""));
 
   for (var i = 1; i < lines.length; i++) {
     const obj = {} as any;
     const currentline = lines[i].split(",");
 
     for (var j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j] ? currentline[j].replaceAll('"', "") : "";
+      obj[headers[j]] = currentline[j] ? currentline[j].replace(/"+/g, "") : "";
     }
 
     jsonArray.push(obj);

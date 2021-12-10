2691986f24e8ff64476431afc2a620fd80c729f6
Kostas Kardatos <kon.kardatos@gmail.com>
2021-12-10 15:22:57 +0200

    generate simple json datamodel

--- nodets/controllers/DataModelController.ts
+++ nodets/controllers/DataModelController.ts
@@ -1,27 +1,12 @@
 import { isArrayHasLength, isObjectHasKeys } from "../helpers/DataTypeCheckers";
 
-export function getDataModelInstanceDisplay(ttdocument: any) {
+export function getDataModelInstanceDisplay(dataModels: any[]) {
   const instances = [] as any[];
-  ttdocument.entities.forEach((entity: any) => {
+  dataModels.forEach((entity: any) => {
     const instance = {};
     if (isObjectHasKeys(entity)) {
       Object.keys(entity).forEach(key => {
-        switch (key) {
-          case "rdf:type":
-            (instance as any)[key] = entity["rdfs:label"];
-            break;
-          case "sh:property":
-            entity["sh:property"].forEach((property: any) => {
-              const propertyName: string = property["sh:path"]?.name || property["sh:path"]["@id"];
-              const value = "sh:node" in property ? { "@id": null } : null;
-              (instance as any)[propertyName] = value;
-            });
-            break;
-
-          default:
-            (instance as any)[key] = "";
-            break;
-        }
+        (instance as any)[key] = "";
       });
     }
     instances.push(instance);
@@ -30,46 +15,31 @@ export function getDataModelInstanceDisplay(ttdocument: any) {
 }
 
 export function generateDataModel(inputJson: any[]) {
-  const dataModel = {
-    "@context": {
-      rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-      im: "http://endhealth.info/im#",
-      graph: "http://graph#",
-      owl: "http://www.w3.org/2002/07/owl#",
-      sh: "http://www.w3.org/ns/shacl#",
-      rdfs: "http://www.w3.org/2000/01/rdf-schema#"
-    },
-    "@graph": "graph:graph",
-    entities: []
-  };
+  const dataModels = [];
 
   if (isArrayHasLength(inputJson)) {
     const exampleRow = inputJson[0];
-    addEntitiesRecursively(dataModel, exampleRow, undefined);
+    addEntitiesRecursively(dataModels, exampleRow, undefined);
   }
 
-  return dataModel;
+  return dataModels;
 }
 
 function addEntitiesRecursively(dataModel: any, exampleRow: any, propertyName: any) {
   const entity = {
-    "@id": propertyName ? "http://graph#" + propertyName : "http://graph#MainEntity",
-    "rdf:type": [
-      {
-        "@id": "sh:NodeShape"
-      }
-    ],
-    "rdfs:label": propertyName ? propertyName : "MainEntity"
+    "@id": "string",
+    type: "string",
+    label: "string"
   };
   if (isObjectHasKeys(exampleRow)) {
     Object.keys(exampleRow).forEach(key => {
       if (isObjectHasKeys(exampleRow[key])) {
         addEntitiesRecursively(dataModel, exampleRow[key], key);
-        entity["im:" + key] = { "@id": "http://graph#" + key };
+        entity[key] = { "@id": "string" };
       } else {
-        entity["im:" + key] = typeof exampleRow[key];
+        entity[key] = typeof exampleRow[key];
       }
     });
   }
-  dataModel.entities.push(entity);
+  dataModel.push(entity);
 }

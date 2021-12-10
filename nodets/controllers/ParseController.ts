dd5f3617b4c2da24d7e3c8646ea1c79fe3088f3b
Kostas Kardatos <kon.kardatos@gmail.com>
2021-12-10 15:22:33 +0200

    fix white space from csv column headers

--- nodets/controllers/ParseController.ts
+++ nodets/controllers/ParseController.ts
@@ -1,14 +1,14 @@
 export function getJsonFromFile(fileString: string): any[] {
   const lines = fileString.split("\n");
   const jsonArray = [] as any[];
-  const headers = lines[0].split(",").map(header => header.replace(/"+/g, ""));
+  const headers = lines[0].split(",").map(header => header.replace(/"+/g, "").trim());
 
   for (var i = 1; i < lines.length; i++) {
     const obj = {} as any;
     const currentline = lines[i].split(",");
 
     for (var j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j] ? currentline[j].replace(/"+/g, "") : "";
+      obj[headers[j]] = currentline[j] ? currentline[j].replace(/"+/g, "").trim() : "";
     }
 
     jsonArray.push(obj);

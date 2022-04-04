d7676aee100bb52976b6aa2efac485ea87b0c68f
Kostas Kardatos <kon.kardatos@gmail.com>
2022-04-04 16:33:15 +0300

    return types for usages

--- api/src/main/java/org/endeavourhealth/imapi/controllers/EntityController.java
+++ api/src/main/java/org/endeavourhealth/imapi/controllers/EntityController.java
@@ -252,7 +252,7 @@ public class EntityController {
 	}
 
 	@GetMapping(value = "/public/usages")
-	public List<TTIriRef> entityUsages(@RequestParam(name = "iri") String iri,
+	public List<TTEntity> entityUsages(@RequestParam(name = "iri") String iri,
 			@RequestParam(name = "page", required = false) Integer page,
 			@RequestParam(name = "size", required = false) Integer size) throws JsonProcessingException {
         LOG.debug("entityUsages");

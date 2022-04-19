f853cb7eb515b5afd5189df4cbc79044a7259eed
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-04-19 14:54:06 +0100

    Implement remaining advanced search functionality

--- node_api/src/controllers/searchController.ts
+++ node_api/src/controllers/searchController.ts
@@ -17,6 +17,7 @@ export default class SearchController {
   async advancedSearch(req: Request, res: Response) {
 
     const searchRequest = req.body;
+    let result = [];
 
     if (searchRequest != null && searchRequest.termFilter) {
       if (!searchRequest.index)
@@ -24,10 +25,21 @@ export default class SearchController {
 
       if (searchRequest.termFilter.length < 3) {
         const qry = this.buildCodeKeyQuery(searchRequest);
-        await this.getEntities(qry, res);
+        result = await this.getEntities(qry);
+      } else if (!searchRequest.termFilter.contains(" ")) {
+        const qry = this.buildSimpleTermCodeMatch(searchRequest);
+        result = await this.getEntities(qry);
+      } else {
+        let qry = this.buildSimpleTermMatch(searchRequest);
+        result = await this.getEntities(qry);
+        if (result.length === 0) {
+          let qry = this.buildMultiWordMatch(searchRequest);
+          result = await this.getEntities(qry);
+        }
       }
     }
 
+    res.setHeader('Content-Type', 'application/json').send(result);
     res.end();
   }
 
@@ -80,6 +92,166 @@ export default class SearchController {
     };
   }
 
+  private buildSimpleTermCodeMatch(searchRequest:any): any {
+    // Fix prefixes if contains ":"
+
+    return {
+      size: 100,
+      query: {
+        function_score: {
+          query: {
+            bool: {
+              filter: this.getFilters(searchRequest),
+              should: [
+                {
+                  match_phrase: {
+                    "termCode.term": {
+                      query: searchRequest.termFilter,
+                      boost: 1.5
+                    }
+                  }
+                },
+                {
+                  match_phrase_prefix: {
+                    "termCode.term": {
+                      query: searchRequest.termFilter,
+                      boost: 0.5
+                    }
+                  }
+                },
+                {
+                  term: {
+                    code: {
+                      value: searchRequest.termFilter,
+                      boost: 2
+                    }
+                  }
+                },
+                {
+                  term: {
+                    iri: {
+                      value: searchRequest.termFilter,
+                      boost: 2
+                    }
+                  }
+                }
+              ],
+              minimum_should_match: 1,
+            },
+          },
+          functions: [
+            {
+              filter: {
+                match_all: {
+                  boost: 1
+                }
+              },
+              field_value_factor: {
+                field: "weighting",
+                factor: 0.5,
+                missing: 1
+              }
+            }
+          ]
+        }
+      }
+    };
+  }
+
+  private buildSimpleTermMatch(searchRequest: any): any {
+    return {
+      size: 100,
+      query: {
+        function_score: {
+          query: {
+            bool: {
+              filter: this.getFilters(searchRequest),
+              should: [
+                {
+                  match_phrase: {
+                    "termCode.term": {
+                      query: searchRequest.termFilter,
+                      boost: 1.5
+                    }
+                  }
+                },
+                {
+                  match_phrase_prefix: {
+                    "termCode.term": {
+                      query: searchRequest.termFilter,
+                      boost: 0.5
+                    }
+                  }
+                }
+              ],
+              minimum_should_match: 1,
+            },
+          },
+          functions: [
+            {
+              filter: {
+                match_all: {
+                  boost: 1
+                }
+              },
+              field_value_factor: {
+                field: "weighting",
+                factor: 0.5,
+                missing: 1
+              }
+            }
+          ],
+        }
+      }
+    };
+  }
+
+  private buildMultiWordMatch(searchRequest: any): any {
+    const musts: any[] = [];
+
+    const words: string[] = searchRequest.termFilter.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+      musts.push({
+        match_phrase: {
+          "termCode.term": {
+            query: words[i],
+            boost: i == 0 ? 4 : 1
+          }
+        }
+      });
+    }
+
+    return {
+      size: 100,
+      query: {
+        function_score: {
+          query: {
+            bool: {
+              filter: this.getFilters(searchRequest),
+              must: musts,
+              minimum_should_match: 1,
+            },
+          },
+          functions: [
+            {
+              filter: {
+                match_all: {
+                  boost: 1
+                }
+              },
+              field_value_factor: {
+                field: "weighting",
+                factor: 0.5,
+                missing: 1
+              }
+            }
+          ],
+        }
+      }
+    };
+  }
+
   private getFilters(searchRequest: any): any {
     const filter: any = [];
     if (searchRequest?.schemeFilter?.length > 0)
@@ -100,34 +272,20 @@ export default class SearchController {
     return result;
   }
 
-  private async getEntities(qry: any, res: Response) {
-    try {
-      const osRes = await axios.post(process.env.OPENSEARCH_URL as string, qry, { headers: { Authorization: 'Basic ' + process.env.OPENSEARCH_AUTH, 'Content-Type': 'application/json' } });
+  private async getEntities(qry: any) {
+    const osRes = await axios.post(process.env.OPENSEARCH_URL as string, qry, { headers: { Authorization: 'Basic ' + process.env.OPENSEARCH_AUTH, 'Content-Type': 'application/json' } });
 
-      if (osRes.status >= 300) {
-        console.error('Error calling opensearch : ' + osRes.status);
-        res.setHeader('Content-Type', 'text/plain').status(500).send('Error calling opensearch : ' + osRes.status);
-      } else {
-
-        const r = osRes.data.hits.hits
-          .map(h => h._source)
-          .map(s => ({
-            iri: s.iri,
-            name: s.name,
-            code: s.code,
-            scheme: s.scheme,
-            status: s.status,
-            entityType: s.entityType,
-            match: s.name
-          }));
-
-        res.setHeader('Content-Type', 'application/json').send(r);
-      }
-    } catch (e) {
-      console.error('Exception calling opensearch : ');
-      console.error(e);
-      res.setHeader('Content-Type', 'text/plain').status(500).send('Exception calling opensearch');
-    }
+    return osRes.data.hits.hits
+      .map(h => h._source)
+      .map(s => ({
+        iri: s.iri,
+        name: s.name,
+        code: s.code,
+        scheme: s.scheme,
+        status: s.status,
+        entityType: s.entityType,
+        match: s.name
+      }));
   }
 }
 

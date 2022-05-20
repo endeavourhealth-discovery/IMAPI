b326c109360a33bbcc749f5a518b3322be649f3a
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-05-20 11:04:00 +0100

    Ignore unknown properties in SearchResultSummary to allow backwards compatibility when adding fields to opensearch

--- api/src/main/java/org/endeavourhealth/imapi/model/search/SearchResultSummary.java
+++ api/src/main/java/org/endeavourhealth/imapi/model/search/SearchResultSummary.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@JsonIgnoreProperties({"id"})
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class SearchResultSummary {
     private String name;

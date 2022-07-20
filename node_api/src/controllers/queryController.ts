30d44cdf89e28f8acd38bfb850edbf202606b138
sheikz13 <contact@appindex.io>
2022-07-20 18:08:06 +0100

    extract clauses from query

--- node_api/src/controllers/queryController.ts
+++ node_api/src/controllers/queryController.ts
@@ -24,6 +24,9 @@ export default class QueryController {
         this.router.get('/node_api/query/public/definition', (req, res, next) => this.definition(req, res, next));
         this.router.get('/node_api/query/public/richDefinition', (req, res, next) => this.richDefinition(req, res, next));
         this.router.get('/node_api/query/public/querySummary', (req, res, next) => this.getQuerySummary(req, res, next))
+        this.router.post('/node_api/query/public/querySummary', (req, res, next) => this.postQuerySummary(req, res, next))
+        this.router.get('/node_api/query/public/queryClauses', (req, res, next) => this.getQueryClauses(req, res, next))
+        this.router.post('/node_api/query/public/queryClauses', (req, res, next) => this.postQueryClauses(req, res, next))
         // this.router.post('/node_api/query/public/querySummary', (req, res, next) => this.postQuerySummary(req, res, next))
         // this.router.post('/node_api/query/public/clauseSummary', (req, res, next) => this.postClauseSummary(req, res, next))
         this.router.get('/node_api/query/public/getSQL', (req, res, next) => this.getSQL(req, res, next))
@@ -75,6 +78,34 @@ export default class QueryController {
       next(e);
     }
   }
+  async postQuerySummary(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await this.workflow.summariseQuery("post", req.body)
+      res.send(data).end();
+    } catch (e) {
+      next(e);
+    }
+  }
+
+
+  async getQueryClauses(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await this.workflow.extractClauses("get", req.query.iri as string)
+      res.send(data).end();
+    } catch (e) {
+      next(e);
+    }
+  }
+  async postQueryClauses(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await this.workflow.extractClauses("post", req.body)
+      res.send(data).end();
+    } catch (e) {
+      next(e);
+    }
+  }
+
+
 
   // async postClauseSummary(req: Request, res: Response) {
   //   console.log(req.body)

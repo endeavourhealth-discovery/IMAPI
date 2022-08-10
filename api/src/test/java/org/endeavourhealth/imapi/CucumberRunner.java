2d01bb27732604decd0abfab2e697d328bf8c081
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-08-10 12:57:45 +0100

    Fix cucumber html output folder

--- api/src/test/java/org/endeavourhealth/imapi/CucumberRunner.java
+++ api/src/test/java/org/endeavourhealth/imapi/CucumberRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    plugin = {"pretty", "html:target/cucumber-report.html"},
+    plugin = {"pretty", "html:build/reports/cucumber/cucumber-report.html"},
     features = {"src/test/resources"}
 )
 public class CucumberRunner {

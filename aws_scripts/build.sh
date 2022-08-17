f872098a231044fd20cdeb206d81d9a68244e8cd
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-08-17 10:43:36 +0100

    Change production build to be parameterised addition of sonar task

--- aws_scripts/build.sh
+++ aws_scripts/build.sh
@@ -25,7 +25,7 @@ aws s3 cp badges s3://endeavour-codebuild-output/badges/${artifact}/ --recursive
 
 # Build
 { #try
-    ./gradlew buildProduction &&
+    ./gradlew build -Penv=prod &&
     buildresult=0
 } || { #catch
     buildresult=1

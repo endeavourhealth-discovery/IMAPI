a7c6700cb038dfad2a571c720a1be04f164265ce
matt-voror <matt@voror.co.uk>
2022-09-08 09:52:23 +0100

    added basic isDefinition validator

--- node_api/src/logic/validator.ts
+++ node_api/src/logic/validator.ts
@@ -8,6 +8,7 @@ export default class Validator {
 
     public validate(iri: string, data: any): boolean {
         if (iri === IM.VALIDATION_HAS_PARENT) return this.hasValidParents(data);
+        if (iri === IM.VALIDATION_IS_DEFINITION) return this.isValidDefinition(data);
         else throw new Error("Validation function: '" + iri + "' was not found in validator.");
     }
 
@@ -17,6 +18,12 @@ export default class Validator {
         if (isObjectHasKeys(data, [IM.IS_CONTAINED_IN]) && isArrayHasLength(data[IM.IS_CONTAINED_IN]) && data[IM.IS_CONTAINED_IN].every((item: any) => isTTIriRef(item))) valid = true;
         if (isObjectHasKeys(data, [RDFS.SUB_PROPERTY_OF]) && isArrayHasLength(data[RDFS.SUB_PROPERTY_OF]) && data[RDFS.SUB_PROPERTY_OF].every((item: any) => isTTIriRef(item))) valid = true;
         if (isObjectHasKeys(data, [IM.IS_CHILD_OF]) && isArrayHasLength(data[IM.IS_CHILD_OF]) && data[IM.IS_CHILD_OF].every((item: any) => isTTIriRef(item))) valid = true;
+        return valid;
+    }
+
+    private isValidDefinition(data: any): boolean {
+        let valid = false;
+        if (isObjectHasKeys(data, [IM.DEFINITION])) valid = true;
         return valid;
     }
 }

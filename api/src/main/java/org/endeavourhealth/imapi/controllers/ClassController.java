56e4353592daccccce49e783c1730a66bbfdda83
Kostas Kardatos <kon.kardatos@gmail.com>
2022-09-29 17:01:47 +0300

    add simplified fielddto call to class properties

--- api/src/main/java/org/endeavourhealth/imapi/controllers/ClassController.java
+++ api/src/main/java/org/endeavourhealth/imapi/controllers/ClassController.java
@@ -1,6 +1,7 @@
 package org.endeavourhealth.imapi.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.endeavourhealth.imapi.model.dto.FieldDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -47,9 +48,41 @@ public class ClassController {
         return ResponseEntity.ok().body(fields);
     }
 
+    @GetMapping(value = "/public/classFields")
+    public ResponseEntity<List<FieldDto>> getClassFields(@RequestParam String className) {
+        LOG.debug("getClass");
+        Class<?> clazz;
+        try {
+            clazz = ClassLoader.getSystemClassLoader().loadClass(className);
+        } catch (ClassNotFoundException classNotFoundException) {
+            return ResponseEntity.notFound().build();
+        }
+        List<Field> fields = new ArrayList<Field>(List.of(clazz.getDeclaredFields()));
+        getDeclaredFieldsRecursively(clazz, fields);
+        Collections.sort(fields, new Comparator<Field>() {
+            @Override
+            public int compare(final Field object1, final Field object2) {
+                return object1.getName().compareTo(object2.getName());
+            }
+        });
+        List<FieldDto> fieldDtos = fields.stream().map(field -> {
+            String firstType = field.getGenericType().getTypeName();
+            String secondType = "";
+            String fullName = field.getGenericType().getTypeName();
+            if(fullName.contains("<")) {
+                String[] nameParts = fullName.split("<");
+                firstType = nameParts[1].replaceFirst(">", "");
+                secondType = nameParts[0];
+            }
+            return new FieldDto(field.getName(), firstType, secondType);
+
+        }).collect(Collectors.toList());
+        return ResponseEntity.ok().body(fieldDtos);
+    }
+
     public void getDeclaredFieldsRecursively(Class<?> clazz, List<Field> fields) {
         Class<?> superclass = clazz.getSuperclass();
-        if(null != superclass) {
+        if (null != superclass) {
             fields.addAll(List.of(superclass.getDeclaredFields()));
             getDeclaredFieldsRecursively(superclass, fields);
         }

e8771b89691a1a9dcdef53c6b21b1fc781771084
matt-voror <matt@voror.co.uk>
2022-09-30 16:39:39 +0100

    added try catches and fixed dates to correctly format.

--- node_api/src/controllers/githubController.ts
+++ node_api/src/controllers/githubController.ts
@@ -20,21 +20,31 @@ export default class GithubController {
     }
 
     private async getLatestRelease(req: Request, res: Response) {
-        const repo = req.query.repositoryName;
-        if (typeof repo !== "string") throw new Error("Missing parameter 'repositoryName' or parameter is not of type 'string'")
-        const result = await this.octokit.request("GET /repos/{owner}/{repo}/releases/latest", {owner: this.owner,repo:repo})
-        const processedResult = this.processRelease(result.data);
-        res.send(processedResult);
+        try {
+            const repo = req.query.repositoryName;
+            if (typeof repo !== "string") throw new Error("Missing parameter 'repositoryName' or parameter is not of type 'string'")
+            const result = await this.octokit.request("GET /repos/{owner}/{repo}/releases/latest", {owner: this.owner,repo:repo})
+            const processedResult = this.processRelease(result.data);
+            res.send(processedResult);
+        } catch(e) {
+            console.log(e)
+            res.end()
+        }
     }
 
     private async getReleases(req: Request, res: Response) {
-        console.log("here")
-        const repo = req.query.repositoryName;
-        if (typeof repo !== "string") throw new Error("Missing parameter 'repositoryName' or parameter is not of type 'string'")
-        const results = await this.octokit.request("GET /repos/{owner}/{repo}/releases", {owner: this.owner,repo: repo})
-        const processedResults: GithubRelease[] = []
-        results.data.forEach((result: any) => processedResults.push(this.processRelease(result)))
-        res.send(processedResults)
+        try {
+            const repo = req.query.repositoryName;
+            if (typeof repo !== "string") throw new Error("Missing parameter 'repositoryName' or parameter is not of type 'string'")
+            const results = await this.octokit.request("GET /repos/{owner}/{repo}/releases", {owner: this.owner,repo: repo})
+            const processedResults: GithubRelease[] = []
+            results.data.forEach((result: any) => processedResults.push(this.processRelease(result)))
+            res.send(processedResults)
+        } catch(e) {
+            console.log(e)
+            res.send([])
+        }
+
     }
 
     private processRelease(release: any): GithubRelease {
--- node_api/src/model/github/GithubRelease.ts
+++ node_api/src/model/github/GithubRelease.ts
@@ -1,8 +1,8 @@
 export default class GithubRelease {
     version: string;
     title: string;
-    createdDate: Date;
-    publishedDate: Date;
+    createdDate: string;
+    publishedDate: string;
     releaseNotes: string[];
     author: string;
     url: string
@@ -10,8 +10,8 @@ export default class GithubRelease {
     constructor(version: string, title: string, createdDate: Date, publishedDate: Date, releaseNotes: string, author: string, url: string) {
         this.version = version;
         this.title = title;
-        this.createdDate = createdDate;
-        this.publishedDate = publishedDate;
+        this.createdDate = this.processDate(new Date(createdDate));
+        this.publishedDate = this.processDate(new Date(publishedDate));
         this.releaseNotes = this.processReleaseNotes(releaseNotes);
         this.author = author;
         this.url = url;
@@ -19,5 +19,9 @@ export default class GithubRelease {
 
     processReleaseNotes(releaseNotes: string): string[] {
       return releaseNotes.split('\r\n').map(item => item.replaceAll("-   ", "").trim());
+    }
+
+    processDate(date: Date): string {
+        return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
     }
 }

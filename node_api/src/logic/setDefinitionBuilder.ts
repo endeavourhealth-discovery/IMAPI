01bfcbc11639b5be9b2058b989611687f5e07df5
Kostas Kardatos <kon.kardatos@gmail.com>
2022-10-07 17:14:38 +0300

    add setController for getting fullset dto

---
+++ node_api/src/logic/setDefinitionBuilder.ts
@@ -0,0 +1,24 @@
+import { QueryDisplay, TTIriRef } from "im-library/dist/types/interfaces/Interfaces";
+import { Enums, Services, Vocabulary, Helpers } from "im-library";
+import axios from "axios";
+import { buildQueryDisplayFromQuery } from "./queryBuilder/displayBuilder";
+
+const { DataTypeCheckers } = Helpers;
+const { isArrayHasLength, isObjectHasKeys, isObject } = DataTypeCheckers;
+const { RDFS, IM } = Vocabulary;
+const { EntityService } = Services;
+const entityService = new EntityService(axios);
+
+export async function buildSetDefinition(iri: string) {
+  const response = await entityService.getPartialEntity(iri, [
+    IM.DEFINITION,
+    IM.HAS_SUBSET,
+    IM.IS_SUBSET_OF,
+    IM.HAS_MEMBER,
+    IM.IS_CONTAINED_IN,
+    RDFS.SUBCLASS_OF
+  ]);
+  const entity = response.data;
+  if (entity[IM.DEFINITION]) entity.queryDisplay = await buildQueryDisplayFromQuery(entity[IM.DEFINITION]);
+  return entity;
+}
--- node_api/src/server.ts
+++ node_api/src/server.ts
@@ -1,36 +1,26 @@
 /// <reference types="vite/client" />
-import App from './app';
+import App from "./app";
 import * as dotenv from "dotenv";
 
-import SearchController from './controllers/searchController';
-
-import EntityController from './controllers/entityController';
-import QueryController from './controllers/queryController';
-import ValidationController from './controllers/validationController';
+import SearchController from "./controllers/searchController";
+import SetController from "./controllers/setController";
+import EntityController from "./controllers/entityController";
+import QueryController from "./controllers/queryController";
+import ValidationController from "./controllers/validationController";
 import GithubController from "./controllers/githubController";
-import bodyParser from 'body-parser';
-import * as dns from 'dns';
+import bodyParser from "body-parser";
+import * as dns from "dns";
 
-dotenv.config({ path: './src/.env' });
+dotenv.config({ path: "./src/.env" });
 
-dns.setDefaultResultOrder('ipv4first');
+dns.setDefaultResultOrder("ipv4first");
 
 const app = new App({
   port: 3000,
-  controllers: [
-    new SearchController(),
-    new EntityController(),
-    new QueryController(),
-    new ValidationController(),
-    new GithubController()
-  ],
-  middleWares: [
-    bodyParser.json(),
-    bodyParser.urlencoded({ extended: true})
-  ],
-})
+  controllers: [new SearchController(), new EntityController(), new QueryController(), new ValidationController(), new GithubController(), new SetController()],
+  middleWares: [bodyParser.json(), bodyParser.urlencoded({ extended: true })]
+});
 
-if (import.meta.env.PROD)
-  app.listen();
+if (import.meta.env.PROD) app.listen();
 
 export const viteNodeApp = app.app;

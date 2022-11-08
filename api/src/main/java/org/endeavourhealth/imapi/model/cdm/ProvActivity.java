2bccda6a01a66fd75e698394792377c581cc7358
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-11-08 13:27:46 +0000

    Fix ProvActivity to be in Prov graph

--- api/src/main/java/org/endeavourhealth/imapi/model/cdm/ProvActivity.java
+++ api/src/main/java/org/endeavourhealth/imapi/model/cdm/ProvActivity.java
@@ -2,6 +2,8 @@ package org.endeavourhealth.imapi.model.cdm;
 
 import org.endeavourhealth.imapi.model.tripletree.*;
 import org.endeavourhealth.imapi.vocabulary.IM;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -9,14 +11,15 @@ import java.util.List;
  * Class which sets and gets Provenance activity entry
  */
 public class ProvActivity extends Entry {
-
+    private static final Logger LOG = LoggerFactory.getLogger(ProvActivity.class);
 	public ProvActivity(){
 		this.addType(IM.PROV_ACIVITY);
+        super.setGraph(IM.GRAPH_PROV);
 	}
 
     @Override
     public ProvActivity setGraph(TTIriRef graph) {
-        super.setGraph(graph);
+        LOG.error("Attempt to set graph on provenance");
         return this;
     }
 

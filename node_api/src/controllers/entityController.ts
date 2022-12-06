032a1a1c9c78687f79a88c46218a56cbd40bc513
Kostas Kardatos <kon.kardatos@gmail.com>
2022-12-06 10:33:06 +0200

    Server error on bundleByPredicateExclusions call Fixes #207

--- node_api/src/controllers/entityController.ts
+++ node_api/src/controllers/entityController.ts
@@ -13,11 +13,26 @@ export default class EntityController {
 
   private initRoutes() {
     this.router.get("/public/detailsDisplay", (req, res, next) => this.getDetailsDisplay(req, res, next));
+    this.router.get("/public/detailsDisplay/loadMore", (req, res, next) => this.loadMoreDetailsTab(req, res, next));
   }
 
   async getDetailsDisplay(req: Request, res: Response, next: NextFunction) {
     try {
       const data = await this.entityService.getDetailsDisplay(req.query.iri as string);
+      res.send(data).end();
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async loadMoreDetailsTab(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await this.entityService.loadMoreDetailsDisplay(
+        req.query.iri as string,
+        req.query.predicate as string,
+        req.query.pageIndex as string,
+        req.query.pageSize as string
+      );
       res.send(data).end();
     } catch (e) {
       next(e);
--- node_api/src/services/entity.service.ts
+++ node_api/src/services/entity.service.ts
@@ -1,9 +1,7 @@
 import Env from "@/services/env.service";
 import { TTBundle, TTIriRef } from "im-library/dist/types/interfaces/Interfaces";
-
-import { Vocabulary } from "im-library/dist/api";
 import { buildDetails } from "@/builders/entity/detailsBuilder";
-
+import { Vocabulary } from "im-library/dist/api";
 const { IM, RDFS, RDF } = Vocabulary;
 export default class EntityService {
   axios: any;
@@ -60,10 +58,58 @@ export default class EntityService {
 
   public async getDetailsDisplay(iri: string): Promise<any[]> {
     try {
-      const response = await this.getBundleByPredicateExclusions(iri, [IM.CODE, RDF.TYPE, RDFS.LABEL, IM.HAS_STATUS, RDFS.COMMENT]);
+      const excludedPredicates = [IM.CODE, RDF.TYPE, RDFS.LABEL, IM.HAS_STATUS, RDFS.COMMENT];
+      const entityPredicates = await this.getPredicates(iri);
+      let response: TTBundle = {} as TTBundle;
+      if (entityPredicates.includes(IM.HAS_MEMBER)) {
+        response = await this.getBundleByPredicateExclusions(iri, excludedPredicates.concat([IM.HAS_MEMBER]));
+        const partialAndCount = await this.getPartialAndTotalCount(iri, IM.HAS_MEMBER, 1, 10);
+        response.entity[IM.HAS_MEMBER] = (partialAndCount.result as any[]).concat([
+          { name: "Load more", "@id": IM.NAMESPACE + "loadMore", totalCount: partialAndCount.totalCount as number }
+        ]);
+        response.predicates[IM.HAS_MEMBER] = "has member";
+      }
       return buildDetails(response);
     } catch (error) {
       return [] as any[];
+    }
+  }
+
+  public async loadMoreDetailsDisplay(iri: string, predicate: string, pageIndex: string, pageSize: string) {
+    try {
+      const response = await this.getPartialAndTotalCount(iri, predicate, parseInt(pageIndex), parseInt(pageSize));
+      const entity = {} as any;
+      entity[predicate] = response.result;
+      const bundle = { entity: entity, predicates: [] } as TTBundle;
+      return buildDetails(bundle);
+    } catch (error) {
+      return [] as any[];
+    }
+  }
+
+  public async getPredicates(iri: string): Promise<string[]> {
+    try {
+      return (
+        await this.axios.get(Env.API + "api/entity/public/predicates", {
+          params: {
+            iri: iri
+          }
+        })
+      ).data;
+    } catch (error) {
+      return [] as string[];
+    }
+  }
+
+  async getPartialAndTotalCount(iri: string, predicate: string, pageIndex: number, pageSize: number): Promise<any> {
+    try {
+      return (
+        await this.axios.get(Env.API + "api/entity/public/partialAndTotalCount", {
+          params: { iri: iri, predicate: predicate, page: pageIndex, size: pageSize }
+        })
+      ).data;
+    } catch (error) {
+      return {} as any;
     }
   }
 }

1bf4bc3a061b0369b9c36c922b38768c81258c62
Richard Collier <gitkraken@tirinoarim.co.uk>
2023-01-19 21:15:38 +0000

    Code smells - Blocker

--- api/src/main/java/org/endeavourhealth/imapi/transformengine/TTTranslator.java
+++ api/src/main/java/org/endeavourhealth/imapi/transformengine/TTTranslator.java
@@ -89,7 +89,7 @@ public class TTTranslator implements SyntaxTranslator {
 	@Override
 	public void setPropertyValue(MapProperty rule, Object targetEntity, String property, Object targetValue) throws DataFormatException {
 		try {
-			if (property.equals("@id") | property.equals("id") | property.equals("iri"))
+			if (property.equals("@id") || property.equals("id") || property.equals("iri"))
 				((TTNode) targetEntity).setIri(((TTLiteral) targetValue).getValue());
 			else {
 				String predicate = property;

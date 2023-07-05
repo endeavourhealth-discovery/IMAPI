e4edcd7587da198f56f55ce805f80f8acd6de9db
David Stables <DavidStables@users.noreply.github.com>
2023-07-05 15:51:51 +0100

    type in RDF vocab

--- api/src/main/java/org/endeavourhealth/imapi/vocabulary/RDF.java
+++ api/src/main/java/org/endeavourhealth/imapi/vocabulary/RDF.java
@@ -8,7 +8,7 @@ public class RDF {
     public static final String NAMESPACE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
     public static final String PREFIX = "rdf";
     public static final TTIriRef TYPE = iri(NAMESPACE + "type");
-    public static final TTIriRef PROPERTY = iri(NAMESPACE + "PropertyRef");
+    public static final TTIriRef PROPERTY = iri(NAMESPACE + "Property");
     public static final TTIriRef LIST = iri(NAMESPACE + "List");
     public static final TTIriRef PREDICATE = iri(NAMESPACE + "predicate");
     public static final TTIriRef SUBJECT = iri(NAMESPACE + "subject");

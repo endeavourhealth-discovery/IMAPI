d8b4fabcb56d4707395ed6a1fae3fa13573166f2
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2024-01-08 12:36:54 +0000

    removed unnecessary imports

--- api/src/main/java/org/endeavourhealth/imapi/controllers/StatusController.java
+++ api/src/main/java/org/endeavourhealth/imapi/controllers/StatusController.java
@@ -1,11 +1,6 @@
 package org.endeavourhealth.imapi.controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import org.endeavourhealth.imapi.logic.service.FunctionService;
-import org.endeavourhealth.imapi.model.iml.FunctionRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;

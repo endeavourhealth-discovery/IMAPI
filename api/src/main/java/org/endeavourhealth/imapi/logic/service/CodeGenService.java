d05ff64791be2a4127c781347ea7dd7d4cb39a04
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2024-03-13 15:23:57 +0000

    added autogen api and necessary vocab for templates

---
+++ api/src/main/java/org/endeavourhealth/imapi/logic/service/CodeGenService.java
@@ -0,0 +1,26 @@
+package org.endeavourhealth.imapi.logic.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.endeavourhealth.imapi.dataaccess.CodeGenRepository;
+import org.endeavourhealth.imapi.model.dto.CodeGenDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+public class CodeGenService {
+    private static final Logger LOG = LoggerFactory.getLogger(CodeGenService.class);
+    private CodeGenRepository codeGenRepository = new CodeGenRepository();
+
+    public List<String> getCodeTemplateList() throws JsonProcessingException {
+        return codeGenRepository.getCodeTemplateList();
+    }
+
+    public CodeGenDto getCodeTemplate(String name) throws JsonProcessingException {
+        return codeGenRepository.getCodeTemplate(name);
+    }
+    public void saveCodeTemplate(String name, String extension, String wrapper, String dataTypeMap, String template) throws JsonProcessingException {
+        codeGenRepository.saveCodeTemplate(name, extension, wrapper, dataTypeMap, template);
+    }
+}

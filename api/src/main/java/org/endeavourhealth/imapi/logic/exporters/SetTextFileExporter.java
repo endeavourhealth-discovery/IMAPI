636f87bebfbdb4bd95a88565d43df7f24da05fb9
David Stables <DavidStables@users.noreply.github.com>
2024-04-11 12:31:03 +0100

    added concept status

--- api/src/main/java/org/endeavourhealth/imapi/logic/exporters/SetTextFileExporter.java
+++ api/src/main/java/org/endeavourhealth/imapi/logic/exporters/SetTextFileExporter.java
@@ -126,14 +126,15 @@ public class SetTextFileExporter {
         String subSet = member.getIsContainedIn() != null ? member.getIsContainedIn().iterator().next().getName() : null;
         String subsetIri = member.getIsContainedIn() != null ? member.getIsContainedIn().iterator().next().getIri() : null;
         String subsetVersion = member.getIsContainedIn() != null ? String.valueOf(member.getIsContainedIn().iterator().next().getVersion()) : "";
+        String status= member.getStatus().getName();
 
         String usage = member.getUsage() != null ? member.getUsage().toString() : null;
         String code= member.getAlternativeCode()==null ?member.getCode(): member.getAlternativeCode();
         if(ownRow) {
             if(includeSubset && subSet != null) {
-                addLineData(del, results, code, member.getName(), scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension);
+                addLineData(del, results, code, member.getName(), status,scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension);
             } else {
-                addLineData(del, results, code, member.getName(), scheme, usage, setName, isExtension);
+                addLineData(del, results, code, member.getName(), status,scheme, usage, setName, isExtension);
             }
         }
         for (Concept legacy: member.getMatchedFrom()) {
@@ -150,19 +151,19 @@ public class SetTextFileExporter {
                 if(im1id && legacy.getIm1Id() != null) {
                     legacy.getIm1Id().forEach(im1 -> {
                         if(includeSubset && subSet != null) {
-                            addLineData(del, results, code, member.getName(), scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension,
+                            addLineData(del, results, code, member.getName(), status,scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension,
                                     legacy.getCode(), legacy.getName(), legacy.getScheme().getIri(), legacyUsage, legacy.getCodeId(),im1);
                         } else {
-                            addLineData(del, results, code, member.getName(), scheme, usage, setName, isExtension,
+                            addLineData(del, results, code, member.getName(), status,scheme, usage, setName, isExtension,
                                     legacy.getCode(), legacy.getName(), legacy.getScheme().getIri(), legacyUsage, legacy.getCodeId(),im1);
                         }
                     });
                 } else {
                     if(includeSubset && subSet != null) {
-                        addLineData(del, results, code, member.getName(), scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension,
+                        addLineData(del, results, code, member.getName(), status,scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension,
                                 legacy.getCode(), legacy.getName(), legacy.getScheme().getIri(), legacyUsage,legacy.getCodeId());
                     } else {
-                        addLineData(del, results, code, member.getName(), scheme, usage, setName, isExtension,
+                        addLineData(del, results, code, member.getName(), status,scheme, usage, setName, isExtension,
                                 legacy.getCode(), legacy.getName(), legacy.getScheme().getIri(), legacyUsage,legacy.getCodeId());
                     }
                 }
@@ -176,19 +177,20 @@ public class SetTextFileExporter {
         String subSet = member.getIsContainedIn() != null ? member.getIsContainedIn().iterator().next().getName() : null;
         String subsetIri = member.getIsContainedIn() != null ? member.getIsContainedIn().iterator().next().getIri() : null;
         String subsetVersion = member.getIsContainedIn() != null ? String.valueOf(member.getIsContainedIn().iterator().next().getVersion()) : "";
+        String status= member.getStatus().getName();
         if(im1id && member.getIm1Id() != null) {
             member.getIm1Id().forEach(im1 -> {
                 if(includeSubset && subSet != null) {
-                    addLineData(del, results, member.getCode(), member.getName(), scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension, im1);
+                    addLineData(del, results, member.getCode(), member.getName(), status,scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension, im1);
                 } else {
-                    addLineData(del, results, member.getCode(), member.getName(), scheme, usage, setName, isExtension, im1);
+                    addLineData(del, results, member.getCode(), member.getName(), status,scheme, usage, setName, isExtension, im1);
                 }
             });
         } else {
             if(includeSubset && subSet != null) {
-                addLineData(del, results, member.getCode(), member.getName(), scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension);
+                addLineData(del, results, member.getCode(), member.getName(), status,scheme, usage, setName, subSet, subsetIri, subsetVersion, isExtension);
             } else {
-                addLineData(del, results, member.getCode(), member.getName(), scheme, usage, setName, isExtension);
+                addLineData(del, results, member.getCode(), member.getName(), status,scheme, usage, setName, isExtension);
             }
         }
 
@@ -211,10 +213,10 @@ public class SetTextFileExporter {
     }
 
     private static String getCoreHeader(String del) {
-        return "code"+ del +"term"+ del + "scheme"+ del + "usage" + del +"set"+ del +"extension";
+        return "code"+ del +"term"+ del + "status"+ del+"scheme"+ del + "usage" + del +"set"+ del +"extension";
     }
     private static String getCoreHeaderWithSubset(String del) {
-        return "code"+ del +"term"+ del + "scheme"+ del + "usage" + del +"set"+ del +"subset"+ del +"subsetIri" + del + "subsetVersion"+ del +"extension";
+        return "code"+ del +"term"+ del + "status"+ del+"scheme"+ del + "usage" + del +"set"+ del +"subset"+ del +"subsetIri" + del + "subsetVersion"+ del +"extension";
     }
 
 

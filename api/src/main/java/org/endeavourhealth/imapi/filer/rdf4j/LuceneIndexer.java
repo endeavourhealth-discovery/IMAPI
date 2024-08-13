d0d95d6b00529c93c2b2ba9558b1b8f719285f78
matt-voror <matt@voror.co.uk>
2024-08-13 09:33:03 +0100

    sonar to textblock changes

--- api/src/main/java/org/endeavourhealth/imapi/filer/rdf4j/LuceneIndexer.java
+++ api/src/main/java/org/endeavourhealth/imapi/filer/rdf4j/LuceneIndexer.java
@@ -6,6 +6,10 @@ import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.eclipse.rdf4j.query.Update;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.endeavourhealth.imapi.dataaccess.helpers.ConnectionManager;
+import org.endeavourhealth.imapi.vocabulary.IM;
+import org.endeavourhealth.imapi.vocabulary.RDF;
+import org.endeavourhealth.imapi.vocabulary.RDFS;
+import org.endeavourhealth.imapi.vocabulary.SHACL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,42 +22,44 @@ public class LuceneIndexer {
   public void buildIndexes() {
     try (RepositoryConnection conn = ConnectionManager.getIMConnection()) {
       dropIndex(conn);
-      String sql = "PREFIX con: <http://www.ontotext.com/connectors/lucene#>\n" +
-        "PREFIX con-inst: <http://www.ontotext.com/connectors/lucene/instance#>\n" +
-        "\n" +
-        "INSERT DATA {\n" +
-        "    con-inst:im_fts con:createConnector '''\n" +
-        "    {\n" +
-        "      \"fields\": [\n" +
-        "        {\n" +
-        "          \"fieldName\": \"label\",  \n" +
-        "          \"propertyChain\": [\n" +
-        "            \"http://www.w3.org/2000/01/rdf-schema#label\"\n" +
-        "          \n" +
-        "          ]\n" +
-        "        },\n" +
-        "{\n" +
-        "    \"fieldName\": \"code\",  \n" +
-        "          \"propertyChain\": [\n" +
-        "             \"http://endhealth.info/im#code\"\n" +
-        "          ],\n" +
-        "     \"datatype\": \"xsd:string\"\n" +
-        "\n" +
-        "     }\n" +
-        "      ],\n" +
-        "\"types\" : [\"http://endhealth.info/im#Concept\",\n" +
-        "            \"http://endhealth.info/im#Folder\",\n" +
-        "          \"http://endhealth.info/im#FormGenerator\",\n" +
-        "        \"http://endhealth.info/im#Function\",\n" +
-        "        \"http://endhealth.info/im#CohortQuery\",\n" +
-        "        \"http://endhealth.info/im#DataSetQuery\",\n" +
-        "        \"http://endhealth.info/im#Query\",\n" +
-        "\t\t\"http://www.w3.org/ns/shacl#NodeShape\",\n" +
-        "        \"http://www.w3.org/1999/02/22-rdf-syntax-ns#Class\",\n" +
-        "        \"http://www.w3.org/1999/02/22-rdf-syntax-ns#Property\"]\n" +
-        "    }\n" +
-        "''' .\n" +
-        "}";
+      String sql = """
+        PREFIX con: <http://www.ontotext.com/connectors/lucene#>
+        PREFIX con-inst: <http://www.ontotext.com/connectors/lucene/instance#>
+
+        INSERT DATA {
+          con-inst:im_fts con:createConnector '''
+            {
+              "fields": [
+                {
+                  "fieldName": "label",
+                  "propertyChain": [
+                    %s
+                  ]
+                },
+                {
+                  "fieldName": "code",
+                  "propertyChain": [
+                     %s
+                  ],
+                  "datatype": "xsd:string"
+                }
+              ],
+              "types" : [
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+              ]
+            }
+          ''' .
+        }
+        """.formatted(RDFS.LABEL, IM.CODE, IM.CONCEPT, IM.FOLDER, IM.FORM_GENERATOR, IM.FUNCTION, IM.COHORT_QUERY, IM.DATASET_QUERY, IM.QUERY, SHACL.NODESHAPE, RDFS.CLASS, RDF.PROPERTY);
       LOG.info("Building lucene index... This will take an hour or so...");
       Update upd = conn.prepareUpdate(sql);
       upd.execute();
@@ -61,29 +67,35 @@ public class LuceneIndexer {
   }
 
   private void dropIndex(RepositoryConnection conn) {
-    String checkList = "PREFIX luc: <http://www.ontotext.com/connectors/lucene#>\n" +
-      "\n" +
-      "SELECT ?cntUri ?cntStr {\n" +
-      "  ?cntUri luc:listConnectors ?cntStr .\n" +
-      "}";
+    String checkList = """
+        PREFIX luc: <http://www.ontotext.com/connectors/lucene#>
+            
+        SELECT ?cntUri ?cntStr {
+          ?cntUri luc:listConnectors ?cntStr .
+        }
+      """;
     TupleQuery qry = conn.prepareTupleQuery(checkList);
     List<String> connectors = new ArrayList<>();
-    TupleQueryResult rs = qry.evaluate();
-    while (rs.hasNext()) {
-      BindingSet bs = rs.next();
-      connectors.add(bs.getValue("cntUri").stringValue());
-    }
+    try (TupleQueryResult rs = qry.evaluate()) {
+      while (rs.hasNext()) {
+        BindingSet bs = rs.next();
+        connectors.add(bs.getValue("cntUri").stringValue());
+      }
 
-    if (connectors.contains("http://www.ontotext.com/connectors/lucene/instance#im_fts")) {
+      if (connectors.contains("http://www.ontotext.com/connectors/lucene/instance#im_fts")) {
 
-      String spq = "PREFIX con: <http://www.ontotext.com/connectors/lucene#>\n" +
-        "PREFIX con-inst: <http://www.ontotext.com/connectors/lucene/instance#>\n" +
-        "\n" +
-        "INSERT DATA {\n" +
-        "    con-inst:im_fts con:dropConnector [].}";
-      LOG.info("Dropping lucene index...");
-      Update upd = conn.prepareUpdate(spq);
-      upd.execute();
+        String spq = """
+            PREFIX con: <http://www.ontotext.com/connectors/lucene#>
+            PREFIX con-inst: <http://www.ontotext.com/connectors/lucene/instance#>
+                  
+            INSERT DATA {
+              con-inst:im_fts con:dropConnector [].
+            }
+          """;
+        LOG.info("Dropping lucene index...");
+        Update upd = conn.prepareUpdate(spq);
+        upd.execute();
+      }
     }
 
   }

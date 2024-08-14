bb47a0e07a304da1ff5aab86281c5c3e2d2f5f59
matt-voror <matt@voror.co.uk>
2024-08-14 15:28:57 +0100

    regex sonar

--- api/src/main/java/org/endeavourhealth/imapi/logic/codegen/PartialDateTime.java
+++ api/src/main/java/org/endeavourhealth/imapi/logic/codegen/PartialDateTime.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 @JsonDeserialize(using = PartialDateTimeDeserializer.class)
 public class PartialDateTime {
 
-  private static final String dateRegex = "([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\\.[0-9]+)?(Z|(\\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?";
+  private static final String dateRegex = "(\\d(\\d(\\d[1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2]\\d|3[0-1])(T([01]\\d|2[0-3]):[0-5]\\d:([0-5]\\d|60)(\\.\\d+)?(Z|(\\+|-)((0\\d|1[0-3]):[0-5]\\d|14:00)))?)?)?";
   private final OffsetDateTime dateTime;
   private final Precision precision;
 

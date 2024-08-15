8321b9866c5266385fd25488489d8385ae0cbfcb
matt-voror <matt@voror.co.uk>
2024-08-15 11:13:58 +0100

    reasoner classes sonar fixes

--- api/src/main/java/org/endeavourhealth/imapi/logic/reasoner/SetBinder.java
+++ api/src/main/java/org/endeavourhealth/imapi/logic/reasoner/SetBinder.java
@@ -4,34 +4,25 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
-import org.endeavourhealth.imapi.dataaccess.EntityTripleRepository;
 import org.endeavourhealth.imapi.dataaccess.SetRepository;
 import org.endeavourhealth.imapi.dataaccess.helpers.ConnectionManager;
 import org.endeavourhealth.imapi.model.iml.Concept;
 import org.endeavourhealth.imapi.model.iml.Entity;
-import org.endeavourhealth.imapi.model.imq.Query;
 import org.endeavourhealth.imapi.model.imq.QueryException;
-import org.endeavourhealth.imapi.model.tripletree.TTBundle;
 import org.endeavourhealth.imapi.model.tripletree.TTNode;
-import org.endeavourhealth.imapi.vocabulary.GRAPH;
 import org.endeavourhealth.imapi.vocabulary.IM;
 import org.endeavourhealth.imapi.vocabulary.RDF;
-import org.endeavourhealth.imapi.vocabulary.SHACL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
-
-import static org.endeavourhealth.imapi.model.tripletree.TTIriRef.iri;
-
 public class SetBinder {
   private static final Logger LOG = LoggerFactory.getLogger(SetBinder.class);
-  private final EntityTripleRepository entityTripleRepository = new EntityTripleRepository();
   private final SetRepository setRepository = new SetRepository();
 
   public void bindSets() throws QueryException, JsonProcessingException {
@@ -41,7 +32,7 @@ public class SetBinder {
     for (String iri : sets) {
       count++;
       if (count % 100 == 0) {
-        LOG.info(count + " sets bound");
+        LOG.info("{} sets bound", count);
       }
       bindSet(iri);
     }
@@ -64,7 +55,7 @@ public class SetBinder {
     return setIris;
   }
 
-  public Set<TTNode> bindSet(String iri) throws JsonProcessingException, QueryException {
+  public Set<TTNode> bindSet(String iri) {
     Set<Concept> members = setRepository.getSomeMembers(iri, 10);
     if (!members.isEmpty()) {
       Set<String> memberIris = members.stream().map(Entity::getIri).collect(Collectors.toSet());
@@ -72,7 +63,7 @@ public class SetBinder {
       setRepository.bindConceptSetToDataModel(iri, dataModels);
       return dataModels;
     }
-    return null;
+    return Collections.emptySet();
   }
 
 }

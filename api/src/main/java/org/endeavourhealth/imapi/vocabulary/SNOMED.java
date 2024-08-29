1cf1c86e09360e79384868a804a122f0fffbd6a7
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2024-08-29 16:04:48 +0100

    updated snomed vocab file

--- api/src/main/java/org/endeavourhealth/imapi/vocabulary/SNOMED.java
+++ api/src/main/java/org/endeavourhealth/imapi/vocabulary/SNOMED.java
@@ -6,6 +6,6 @@ public class SNOMED {
     public static final String DOMAIN = "http://snomed.info/";
     public static final String NAMESPACE = SNOMED.DOMAIN + "sct#";
     public static final String PREFIX = "sn";
-    public static final String ATTRIBUTE = SNOMED.DOMAIN + "246061005";
+    public static final String ATTRIBUTE = SNOMED.NAMESPACE + "246061005";
     public static final String ANY = SNOMED.NAMESPACE + "*";
 }

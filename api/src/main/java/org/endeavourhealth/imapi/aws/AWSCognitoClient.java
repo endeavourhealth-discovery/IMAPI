90e604ea95b2fc4a46a538b08c37c0f6d21d10da
matt-voror <matt@voror.co.uk>
2024-09-02 16:49:05 +0100

    restored caching result

--- api/src/main/java/org/endeavourhealth/imapi/aws/AWSCognitoClient.java
+++ api/src/main/java/org/endeavourhealth/imapi/aws/AWSCognitoClient.java
@@ -14,46 +14,46 @@ import java.util.Map;
 import java.util.Objects;
 
 public class AWSCognitoClient {
-    private final AWSCognitoIdentityProvider identityProvider;
-    private static final Map<String, String> userCache = new HashMap<>();
+  private static final Map<String, String> userCache = new HashMap<>();
+  private final AWSCognitoIdentityProvider identityProvider;
 
-    public AWSCognitoClient() {
-        this.identityProvider = createCognitoClient();
-    }
+  public AWSCognitoClient() {
+    this.identityProvider = createCognitoClient();
+  }
 
-    private AWSCognitoIdentityProvider createCognitoClient() {
-        AWSCredentials cred = new BasicAWSCredentials(System.getenv("AWS_ACCESS_KEY"), System.getenv("AWS_SECRET_KEY"));
-        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(cred);
-        return AWSCognitoIdentityProviderClientBuilder.standard().withCredentials(credentialsProvider).withRegion(System.getenv("COGNITO_REGION")).build();
-    }
+  private AWSCognitoIdentityProvider createCognitoClient() {
+    AWSCredentials cred = new BasicAWSCredentials(System.getenv("AWS_ACCESS_KEY"), System.getenv("AWS_SECRET_ACCESS_KEY"));
+    AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(cred);
+    return AWSCognitoIdentityProviderClientBuilder.standard().withCredentials(credentialsProvider).withRegion(System.getenv("COGNITO_REGION")).build();
+  }
 
-    public String adminGetUser(String id) throws UserNotFoundException {
-        String username = userCache.get(id);
-//        if (null != username) return username;
-        ListUsersRequest usersRequest = new ListUsersRequest().withUserPoolId(System.getenv("COGNITO_USER_POOL")).withAttributesToGet("sub").withFilter("sub = \"" + id + "\"");
-        ListUsersResult result = identityProvider.listUsers(usersRequest);
-        if (result.getUsers().isEmpty()) throw new UserNotFoundException("User with id: " + id + " not found.");
-        username = result.getUsers().get(0).getUsername();
-        userCache.put(id, username);
-        return username;
-    }
+  public String adminGetUser(String id) throws UserNotFoundException {
+    String username = userCache.get(id);
+    if (null != username) return username;
+    ListUsersRequest usersRequest = new ListUsersRequest().withUserPoolId(System.getenv("COGNITO_USER_POOL")).withAttributesToGet("sub").withFilter("sub = \"" + id + "\"");
+    ListUsersResult result = identityProvider.listUsers(usersRequest);
+    if (result.getUsers().isEmpty()) throw new UserNotFoundException("User with id: " + id + " not found.");
+    username = result.getUsers().get(0).getUsername();
+    userCache.put(id, username);
+    return username;
+  }
 
-    public String adminGetId(String username) throws UserNotFoundException {
-        String id = getKeysByValue(username);
-        if (null != id) return id;
-        ListUsersRequest usersRequest = new ListUsersRequest().withUserPoolId(System.getenv("COGNITO_USER_POOL")).withAttributesToGet("sub").withFilter("username = \"" + username + "\"");
-        ListUsersResult result = identityProvider.listUsers(usersRequest);
-        if (result.getUsers().isEmpty()) throw new UserNotFoundException("User with username: " + username + " not found.");
-        id = result.getUsers().get(0).getAttributes().get(0).getValue();
-        userCache.put(id, username);
-        return id;
-    }
+  public String adminGetId(String username) throws UserNotFoundException {
+    String id = getKeysByValue(username);
+    if (null != id) return id;
+    ListUsersRequest usersRequest = new ListUsersRequest().withUserPoolId(System.getenv("COGNITO_USER_POOL")).withAttributesToGet("sub").withFilter("username = \"" + username + "\"");
+    ListUsersResult result = identityProvider.listUsers(usersRequest);
+    if (result.getUsers().isEmpty()) throw new UserNotFoundException("User with username: " + username + " not found.");
+    id = result.getUsers().get(0).getAttributes().get(0).getValue();
+    userCache.put(id, username);
+    return id;
+  }
 
-    private String getKeysByValue(String value) {
-        for (Map.Entry<String, String> entry : AWSCognitoClient.userCache.entrySet()) {
-            if (Objects.equals(value, entry.getValue())) return entry.getKey();
-        }
-        return null;
+  private String getKeysByValue(String value) {
+    for (Map.Entry<String, String> entry : AWSCognitoClient.userCache.entrySet()) {
+      if (Objects.equals(value, entry.getValue())) return entry.getKey();
     }
+    return null;
+  }
 }
 

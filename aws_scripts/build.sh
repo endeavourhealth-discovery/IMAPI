f525452f8efa9bbc9564cacb3f6cb0116355db70
Richard Collier <rich@voror.co.uk>
2024-10-28 13:36:07 +0000

    Maven publishing fix

--- aws_scripts/build.sh
+++ aws_scripts/build.sh
@@ -25,7 +25,7 @@ aws s3 cp badges s3://endeavour-codebuild-output/badges/${artifact}/ --recursive
 
 # Build
 { #try
-    ./gradlew publish &&
+    ./gradlew build &&
     buildresult=0
 } || { #catch
     buildresult=1

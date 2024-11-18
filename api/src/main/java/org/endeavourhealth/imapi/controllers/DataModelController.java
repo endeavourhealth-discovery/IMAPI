91ad1899964ce683e6a23adeabc56658e3a9dfd3
David Stables <DavidStables@users.noreply.github.com>
2024-11-18 11:42:39 +0000

    DataModel Display API

--- api/src/main/java/org/endeavourhealth/imapi/controllers/DataModelController.java
+++ api/src/main/java/org/endeavourhealth/imapi/controllers/DataModelController.java
@@ -8,6 +8,7 @@ import org.endeavourhealth.imapi.logic.service.DataModelService;
 import org.endeavourhealth.imapi.logic.service.EntityService;
 import org.endeavourhealth.imapi.logic.service.RequestObjectService;
 import org.endeavourhealth.imapi.logic.service.SetService;
+import org.endeavourhealth.imapi.model.iml.NodeShape;
 import org.endeavourhealth.imapi.model.tripletree.TTEntity;
 import org.endeavourhealth.imapi.model.tripletree.TTIriRef;
 import org.endeavourhealth.imapi.utility.MetricsHelper;
@@ -31,10 +32,12 @@ public class DataModelController {
   private static final Logger LOG = LoggerFactory.getLogger(DataModelController.class);
 
   @GetMapping("/public/dataModelProperties")
-  public TTEntity getDataModelProperties(@RequestParam(name = "iri") String iri, @RequestParam(name = "parent", required = false) String parent) throws IOException {
+  public NodeShape getDataModelProperties(@RequestParam(name = "iri") String iri, @RequestParam(name = "parent", required = false) String parent) throws IOException {
     try (MetricsTimer t = MetricsHelper.recordTime("API.Entity.DataModelProperties.GET")) {
       LOG.debug("getDataModelProperties");
-      return dataModelService.getDataModelPropertiesAndSubClasses(iri, parent);
+     // TTEntity entity=dataModelService.getDataModelPropertiesAndSubClasses(iri, parent);
+      NodeShape shape= dataModelService.getDataModelDisplayProperties(iri);
+      return shape;
     }
   }
 
@@ -46,11 +49,11 @@ public class DataModelController {
     }
   }
 
-  @GetMapping(value = "/public/dataModels")
-  public List<TTIriRef> getDataModelsFromProperty(@RequestParam(name = "propIri") String propIri) {
-    LOG.debug("getDataModelsFromProperty");
-    return dataModelService.getDataModelsFromProperty(propIri);
-  }
+//  @GetMapping(value = "/public/dataModels")
+//  public List<TTIriRef> getDataModelsFromProperty(@RequestParam(name = "propIri") String propIri) {
+//    LOG.debug("getDataModelsFromProperty");
+//    return dataModelService.getDataModelsFromProperty(propIri);
+//  }
 
   @GetMapping(value = "public/checkPropertyType")
   public String checkPropertyType(@RequestParam(name = "propertyIri") String iri) throws IOException {

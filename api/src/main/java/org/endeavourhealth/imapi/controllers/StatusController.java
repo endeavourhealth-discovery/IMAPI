2c2b3c502a153f919dc965818b3fa78044d4b08d
Richard Collier <rich@voror.co.uk>
2025-01-02 11:46:49 +0000

    Server "public mode" switch

--- api/src/main/java/org/endeavourhealth/imapi/controllers/StatusController.java
+++ api/src/main/java/org/endeavourhealth/imapi/controllers/StatusController.java
@@ -1,12 +1,16 @@
 package org.endeavourhealth.imapi.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.endeavourhealth.imapi.utility.EnvHelper;
 import org.endeavourhealth.imapi.utility.MetricsHelper;
 import org.endeavourhealth.imapi.utility.MetricsTimer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.io.IOException;
@@ -24,6 +28,14 @@ public class StatusController {
     try (MetricsTimer t = MetricsHelper.recordTime("API.Status.HealthCheck.GET")) {
       LOG.debug("healthCheck");
       return ResponseEntity.ok("OK");
+    }
+  }
+
+  @GetMapping("/public/isPublicMode")
+  public ResponseEntity<Boolean> isPublicMode() throws IOException {
+    try (MetricsTimer t = MetricsHelper.recordTime("API.Status.isPublicMode.GET")) {
+      LOG.debug("isPublicMode");
+      return ResponseEntity.ok(EnvHelper.isPublicMode());
     }
   }
 }

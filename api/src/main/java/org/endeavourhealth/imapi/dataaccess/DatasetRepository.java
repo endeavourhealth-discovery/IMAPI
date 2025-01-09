d10cb34f7844bf443459bb76e3d43260a01375cc
matt-voror <matt@voror.co.uk>
2025-01-09 11:52:52 +0000

    dataset properties search fix for sub datasets

--- api/src/main/java/org/endeavourhealth/imapi/dataaccess/DatasetRepository.java
+++ api/src/main/java/org/endeavourhealth/imapi/dataaccess/DatasetRepository.java
@@ -22,26 +22,42 @@ public class DatasetRepository {
     List<TTIriRef> results = new ArrayList<>();
 
     String spql = """
-      SELECT ?propertyLabel ?propertyPath WHERE {
-        ?dataModel sh:property ?property .
-        ?property sh:path ?propertyPath .
-        ?propertyPath rdfs:label ?propertyLabel .
-        filter contains(?propertyLabel, ?searchTerm)
+      SELECT DISTINCT ?label ?path WHERE {
+         {
+             ?dataModel sh:property ?property .
+             ?property sh:path ?propertyPath .
+             ?propertyPath rdfs:label ?propertyLabel .
+             BIND(?propertyLabel AS ?label)
+             BIND(?propertyPath AS ?path)
+         }
+         UNION {
+             ?property sh:node ?subDataModel .
+             ?subDataModel sh:property ?subProperty .
+             ?subProperty sh:path ?subPropertyPath .
+             ?subPropertyPath rdfs:label ?subPropertyLabel .
+             BIND(?subPropertyLabel AS ?label)
+             BIND(?subPropertyPath AS ?path)
+         }
+         UNION {
+             ?subProperty sh:node ?sub2DataModel .
+             ?sub2DataModel sh:property ?sub2Property .
+             ?sub2Property sh:path ?sub2PropertyPath .
+             ?sub2PropertyPath rdfs:label ?sub2PropertyLabel .
+             BIND(?sub2PropertyLabel AS ?label)
+             BIND(?sub2PropertyPath AS ?path)
+         }
+         FILTER CONTAINS(?label, ?searchTerm)
       }
-      LIMIT %s
-      OFFSET %s
-      """.formatted(size, rowNumber);
+      """;
 
     try (RepositoryConnection conn = ConnectionManager.getIMConnection()) {
       TupleQuery qry = prepareSparql(conn, spql);
       qry.setBinding("dataModel", iri(datamodelIri));
       qry.setBinding("searchTerm", literal(searchTerm));
-      qry.setBinding("size", literal(size));
-      qry.setBinding("rowNumber", literal(rowNumber));
       try (TupleQueryResult rs = qry.evaluate()) {
         while (rs.hasNext()) {
           BindingSet bindingSet = rs.next();
-          results.add(TTIriRef.iri(bindingSet.getValue("propertyPath").stringValue(), bindingSet.getValue("propertyLabel").stringValue()));
+          results.add(TTIriRef.iri(bindingSet.getValue("path").stringValue(), bindingSet.getValue("label").stringValue()));
         }
       }
     }

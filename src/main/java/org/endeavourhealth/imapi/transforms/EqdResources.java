64058180f08f70840e0cf7d12500b3ecd22646c8
David Stables <DavidStables@users.noreply.github.com>
2025-07-21 11:39:42 +0100

    eqd converter

--- src/main/java/org/endeavourhealth/imapi/transforms/EqdResources.java
+++ src/main/java/org/endeavourhealth/imapi/transforms/EqdResources.java
@@ -212,6 +212,7 @@ public class EqdResources {
         TTEntity baseEntity= new TTEntity()
           .setIri(this.namespace + "BaseCriteria_"+ baseHash)
           .setName("Base events ("+baseCounter+") (IndexEvents)")
+          .setScheme(iri(namespace))
           .addType(iri(IM.QUERY));
         baseEntity.set(IM.DEFINITION,TTLiteral.literal(baseMatch));
         baseMatchMap.put(baseHash, baseEntity.getIri());
@@ -285,8 +286,6 @@ public class EqdResources {
       this.setBaseReturn(baseMatch);
     }
 
-
-    baseMatch.setIri(namespace.toString() + UUID.nameUUIDFromBytes(baseContent.getBytes()));
     if (baseMatch.getOr() != null && baseMatch.getOr().size() == 2) {
       Match lastMatch = baseMatch.getOr().getLast();
       baseMatch.setOrderBy(lastMatch.getOrderBy());
@@ -1087,6 +1086,7 @@ public class EqdResources {
 
   private void createLibraryValueSet(String iri, String name) {
     TTEntity valueSet = (new TTEntity()).setIri(iri).setName(name).addType(iri(IM.CONCEPT_SET));
+    valueSet.setScheme(iri(namespace));
     this.addUsedIn(valueSet);
     this.document.addEntity(valueSet);
   }
@@ -1111,6 +1111,7 @@ public class EqdResources {
       TTEntity valueSet = new TTEntity()
         .setIri(namespace + vs.getId())
         .setName(name)
+        .setScheme(iri(namespace))
         .addType(iri(IM.CONCEPT_SET))
         .set(iri(IM.DEFINITION), TTLiteral.literal(definition));
       this.addUsedIn(valueSet);
@@ -1136,6 +1137,7 @@ public class EqdResources {
     } else {
       TTEntity valueSet = new TTEntity()
         .setIri(namespace + vs.getId()).setName(name).addType(iri(IM.CONCEPT_SET));
+      valueSet.setScheme(iri(namespace));
       if (this.columnGroup != null) {
         valueSet.addObject(iri(IM.USED_IN), iri(this.columnGroup.getIri()));
       } else {

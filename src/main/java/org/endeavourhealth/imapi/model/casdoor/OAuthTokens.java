6ac6e1389d846529c72d7df17cd3c35b741939af
matt-voror <matt@voror.co.uk>
2026-01-12 22:13:12 +0000

    added oauthtokens Model

--- src/main/java/org/endeavourhealth/imapi/model/casdoor/OAuthTokens.java
+++ src/main/java/org/endeavourhealth/imapi/model/casdoor/OAuthTokens.java
@@ -1,4 +1,19 @@
 package org.endeavourhealth.imapi.model.casdoor;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OAuthTokens {
+  String access_token;
+  String id_token;
+  String refresh_token;
+  String token_type;
+  String scope;
+  String expires_in;
 }

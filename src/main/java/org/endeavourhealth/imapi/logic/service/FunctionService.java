08f3d6cd4f01daedede6e2e058b49334233436bb
matt-voror <matt@voror.co.uk>
2026-02-05 21:11:57 +0000

    Get editableSchemes fix following namespace refactor

--- src/main/java/org/endeavourhealth/imapi/logic/service/FunctionService.java
+++ src/main/java/org/endeavourhealth/imapi/logic/service/FunctionService.java
@@ -129,7 +129,7 @@ public class FunctionService {
   private JsonNode getUserEditableSchemes(HttpServletRequest request) throws JsonProcessingException, UserNotFoundException {
     List<EntityReferenceNode> results = entityService.getImmediateChildren(IM.NAMESPACE.toString(), null, 1, 200, false);
     User user = securityService.getUser(request);
-    List<TTIriRef> resultsAsIri = results.stream().filter(r -> user.getNamespaces().stream().anyMatch(o -> o.getIri().equals(r.getIri()))).map(r -> new TTIriRef(r.getIri(), r.getName())).toList();
+    List<TTIriRef> resultsAsIri = results.stream().filter(r -> user.getNamespaces().stream().anyMatch(o -> o.getIri().toString().equals(r.getIri()))).map(r -> new TTIriRef(r.getIri(), r.getName())).toList();
     try (CachedObjectMapper om = new CachedObjectMapper()) {
       return om.valueToTree(resultsAsIri);
     }

748afe60eb828777e026a4ef8f5dd33fa2bcfbeb
matt-voror <matt@voror.co.uk>
2026-04-30 13:58:43 +0100

    missing noargs

--- src/main/java/org/endeavourhealth/imapi/model/postRequestPrimatives/REPOBody.java
+++ src/main/java/org/endeavourhealth/imapi/model/postRequestPrimatives/REPOBody.java
@@ -1,9 +1,11 @@
 package org.endeavourhealth.imapi.model.postRequestPrimatives;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.endeavourhealth.imapi.model.github.REPO;
 
+@NoArgsConstructor
 @Getter
 @Setter
 public class REPOBody {

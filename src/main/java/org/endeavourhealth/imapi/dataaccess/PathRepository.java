805858987c3c248a4f500e586d553e12a66ea913
David Stables <DavidStables@users.noreply.github.com>
2026-05-27 14:57:29 +0100

    Update PathRepository.java

--- src/main/java/org/endeavourhealth/imapi/dataaccess/PathRepository.java
+++ src/main/java/org/endeavourhealth/imapi/dataaccess/PathRepository.java
@@ -23,6 +23,7 @@ public class PathRepository {
     }
     return document;
   }
+  
 
   private List<Match> getPaths(IMDB conn, String source, String target) {
     List<Match> result = new ArrayList<>();

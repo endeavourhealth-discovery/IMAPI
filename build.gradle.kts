ad8fdc9ac3596b9ac144522996bf0046ec6501b9
kkardatos <kon.kardatos@gmail.com>
2026-08-25 09:58:23 +0100

    fix ast conversion + add local test for IMQClinicalIndicatorsTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -171,7 +171,7 @@ repositories {
 tasks.test {
   jvmArgs("-XX:+EnableDynamicAgentLoading")
   useJUnitPlatform {
-    excludeTags("IMQTest", "IMQFullTest")
+    excludeTags("IMQTest", "IMQFullTest", "IMQClinicalIndicatorsTest")
   }
   finalizedBy("jacocoTestReport")
 }
